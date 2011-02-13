727303bba73b2a40397ee673e88b599b9d3a3142
s_cadwallader <steve.cadwallader@gmail.com>
2011-02-13 17:47:20 -0500

    Update to v0.3.5 label

--- CodeMaid/VSPackage.Designer.cs
+++ CodeMaid/VSPackage.Designer.cs
@@ -8,8 +8,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace SteveCadwallader.CodeMaid
-{
+namespace SteveCadwallader.CodeMaid {
+    using System;
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -20,262 +22,215 @@ namespace SteveCadwallader.CodeMaid
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "2.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class VSPackage
-    {
+    internal class VSPackage {
+        
         private static global::System.Resources.ResourceManager resourceMan;
-
+        
         private static global::System.Globalization.CultureInfo resourceCulture;
-
+        
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal VSPackage()
-        {
+        internal VSPackage() {
         }
-
+        
         /// <summary>
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager
-        {
-            get
-            {
-                if (object.ReferenceEquals(resourceMan, null))
-                {
+        internal static global::System.Resources.ResourceManager ResourceManager {
+            get {
+                if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SteveCadwallader.CodeMaid.VSPackage", typeof(VSPackage).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
             }
         }
-
+        
         /// <summary>
         ///   Overrides the current thread's CurrentUICulture property for all
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture
-        {
-            get
-            {
+        internal static global::System.Globalization.CultureInfo Culture {
+            get {
                 return resourceCulture;
             }
-            set
-            {
+            set {
                 resourceCulture = value;
             }
         }
-
+        
         /// <summary>
-        ///   Looks up a localized string similar to P8QRQ8KIAKPMMPRHRZJTAAJHAICDJJHTCKMKDZZQHKJHHIZJIKIRIQR2A9CTPRRPRKHJJIQ9QJCCMCM2RIHCJ1KDJ3ZACJEPMRDJCDZHITH3MTQTQRDAP2DCPQATIADJ.
+        ///   Looks up a localized string similar to KRHQZRA2CZK9QTEIP8KTM8QRAZITDACIJ2MQIDPMMDEMHIMCRHEQA1M0C3CEZEQAI1ZCKCJTMMKHR1DMCMKJM8ADZKQZHZIZECQQH3Q2ZIJIJ0RPETPCEHE8Q3IKJHPM.
         /// </summary>
-        internal static string _1
-        {
-            get
-            {
+        internal static string _1 {
+            get {
                 return ResourceManager.GetString("1", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to CodeMaid.
         /// </summary>
-        internal static string _110
-        {
-            get
-            {
+        internal static string _110 {
+            get {
                 return ResourceManager.GetString("110", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to CodeMaid is a free and open source Visual Studio 2005/2008/2010 package that handles some of your repetitive work for you.  Code cleanup (C#/C++/XAML/XML/ASP/HTML/CSS/JavaScript), extended build status, complexity calculations and miscellaneous other features that make C# development in Visual Studio even easier.
         ///
         ///Written by Steve Cadwallader, for more information visit http://www.bitbucket.org/s_cadwallader/codemaid/.
         /// </summary>
-        internal static string _112
-        {
-            get
-            {
+        internal static string _112 {
+            get {
                 return ResourceManager.GetString("112", resourceCulture);
             }
         }
-
+        
         /// <summary>
-        ///   Looks up a localized string similar to 0.3.4.
+        ///   Looks up a localized string similar to 0.3.5.
         /// </summary>
-        internal static string _114
-        {
-            get
-            {
+        internal static string _114 {
+            get {
                 return ResourceManager.GetString("114", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to CodeMaid.
         /// </summary>
-        internal static string _116
-        {
-            get
-            {
+        internal static string _116 {
+            get {
                 return ResourceManager.GetString("116", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to General.
         /// </summary>
-        internal static string _118
-        {
-            get
-            {
+        internal static string _118 {
+            get {
                 return ResourceManager.GetString("118", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to Insert.
         /// </summary>
-        internal static string _120
-        {
-            get
-            {
+        internal static string _120 {
+            get {
                 return ResourceManager.GetString("120", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to Remove.
         /// </summary>
-        internal static string _122
-        {
-            get
-            {
+        internal static string _122 {
+            get {
                 return ResourceManager.GetString("122", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to Update.
         /// </summary>
-        internal static string _124
-        {
-            get
-            {
+        internal static string _124 {
+            get {
                 return ResourceManager.GetString("124", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to Build Status.
         /// </summary>
-        internal static string _126
-        {
-            get
-            {
+        internal static string _126 {
+            get {
                 return ResourceManager.GetString("126", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to Snooper.
         /// </summary>
-        internal static string _128
-        {
-            get
-            {
+        internal static string _128 {
+            get {
                 return ResourceManager.GetString("128", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to CodeMaid cleanup options (general).
         /// </summary>
-        internal static string _130
-        {
-            get
-            {
+        internal static string _130 {
+            get {
                 return ResourceManager.GetString("130", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to CodeMaid cleanup options (insert).
         /// </summary>
-        internal static string _132
-        {
-            get
-            {
+        internal static string _132 {
+            get {
                 return ResourceManager.GetString("132", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to CodeMaid cleanup options (remove).
         /// </summary>
-        internal static string _134
-        {
-            get
-            {
+        internal static string _134 {
+            get {
                 return ResourceManager.GetString("134", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to CodeMaid cleanup options (update).
         /// </summary>
-        internal static string _136
-        {
-            get
-            {
+        internal static string _136 {
+            get {
                 return ResourceManager.GetString("136", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to CodeMaid build status options.
         /// </summary>
-        internal static string _138
-        {
-            get
-            {
+        internal static string _138 {
+            get {
                 return ResourceManager.GetString("138", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to CodeMaid snooper options.
         /// </summary>
-        internal static string _140
-        {
-            get
-            {
+        internal static string _140 {
+            get {
                 return ResourceManager.GetString("140", resourceCulture);
             }
         }
-
-        internal static System.Drawing.Icon _400
-        {
-            get
-            {
+        
+        internal static System.Drawing.Icon _400 {
+            get {
                 object obj = ResourceManager.GetObject("400", resourceCulture);
                 return ((System.Drawing.Icon)(obj));
             }
         }
-
-        internal static System.Drawing.Bitmap _501
-        {
-            get
-            {
+        
+        internal static System.Drawing.Bitmap _501 {
+            get {
                 object obj = ResourceManager.GetObject("501", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-
-        internal static System.Drawing.Bitmap _502
-        {
-            get
-            {
+        
+        internal static System.Drawing.Bitmap _502 {
+            get {
                 object obj = ResourceManager.GetObject("502", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
