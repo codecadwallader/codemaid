9505b62cca0b9c504a08dfff178de61889ef4d10
s_cadwallader <steve.cadwallader@gmail.com>
2011-12-02 21:29:32 -0500

    Add a stub class CodeReorderHelper.

---
+++ CodeMaid/Helpers/CodeReorderHelper.cs
@@ -0,0 +1,41 @@
+﻿#region CodeMaid is Copyright 2007-2011 Steve Cadwallader.
+
+// CodeMaid is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License version 3
+// as published by the Free Software Foundation.
+//
+// CodeMaid is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details <http://www.gnu.org/licenses/>.
+
+#endregion CodeMaid is Copyright 2007-2011 Steve Cadwallader.
+
+using SteveCadwallader.CodeMaid.CodeItems;
+
+namespace SteveCadwallader.CodeMaid.Helpers
+{
+    /// <summary>
+    /// A helper class for reordering code.
+    /// </summary>
+    internal class CodeReorderHelper
+    {
+        /// <summary>
+        /// Moves the specified item above the specified base.
+        /// </summary>
+        /// <param name="itemToMove">The item to move.</param>
+        /// <param name="baseItem">The base item.</param>
+        internal void MoveAbove(BaseCodeItem itemToMove, BaseCodeItem baseItem)
+        {
+        }
+
+        /// <summary>
+        /// Moves the specified item below the specified base.
+        /// </summary>
+        /// <param name="itemToMove">The item to move.</param>
+        /// <param name="baseItem">The base item.</param>
+        internal void MoveBelow(BaseCodeItem itemToMove, BaseCodeItem baseItem)
+        {
+        }
+    }
+}
