d14d7e4115d5fd82699b97a5d83e487121b73b37
s_cadwallader <steve.cadwallader@gmail.com>
2012-07-10 21:34:20 -0400

    Update to v0.5.3 label.

--- CodeMaid/Properties/AssemblyInfo.cs
+++ CodeMaid/Properties/AssemblyInfo.cs
@@ -15,5 +15,5 @@ using System.Runtime.InteropServices;
 [assembly: CLSCompliant(false)]
 [assembly: NeutralResourcesLanguage("en-US")]
 
-[assembly: AssemblyVersion("0.5.2.1")]
-[assembly: AssemblyFileVersion("0.5.2.1")]
+[assembly: AssemblyVersion("0.5.3.0")]
+[assembly: AssemblyFileVersion("0.5.3.0")]
