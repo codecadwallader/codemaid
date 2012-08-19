35942c9a223fb2ad36b1ea47083485fda27a915f
s_cadwallader <steve.cadwallader@gmail.com>
2012-08-19 09:36:32 -0400

    Extend remove blank lines after attributes regular expressions to make an exception for single line comments.  Issue 124.

--- CodeMaid/Logic/Cleaning/RemoveWhitespaceLogic.cs
+++ CodeMaid/Logic/Cleaning/RemoveWhitespaceLogic.cs
@@ -106,8 +106,8 @@ namespace SteveCadwallader.CodeMaid.Logic.Cleaning
             if (!Settings.Default.Cleaning_RemoveBlankLinesAfterAttributes) return;
 
             string pattern = _package.UsePOSIXRegEx
-                                 ? @"\]{:b*(//.*)*}\n\n"
-                                 : @"\]([ \t]*(//[^\r\n]*)*)(\r?\n){2,}";
+                                 ? @"\]{:b*(//.*)*}\n\n(?!:b*//)"
+                                 : @"\]([ \t]*(//[^\r\n]*)*)(\r?\n){2}(?![ \t]*//)";
             string replacement = _package.UsePOSIXRegEx
                                      ? @"\]\1" + Environment.NewLine
                                      : @"]$1" + Environment.NewLine;
