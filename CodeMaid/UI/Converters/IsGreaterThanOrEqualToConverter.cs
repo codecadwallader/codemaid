66e5fe45ba52451760e7565a40b1c898ebbe231a
s_cadwallader <steve.cadwallader@gmail.com>
2012-09-15 10:28:58 -0400

    Update theme styling for complexity colors.

---
+++ CodeMaid/UI/Converters/IsGreaterThanOrEqualToConverter.cs
@@ -0,0 +1,71 @@
+﻿#region CodeMaid is Copyright 2007-2012 Steve Cadwallader.
+
+// CodeMaid is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License version 3
+// as published by the Free Software Foundation.
+//
+// CodeMaid is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details <http://www.gnu.org/licenses/>.
+
+#endregion CodeMaid is Copyright 2007-2012 Steve Cadwallader.
+
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace SteveCadwallader.CodeMaid.UI.Converters
+{
+    /// <summary>
+    /// A converter for performing is greater than or equal to comparisons between two specified values.
+    /// </summary>
+    public class IsGreaterThanOrEqualToConverter : IMultiValueConverter
+    {
+        /// <summary>
+        /// A default instance of the <see cref="IsGreaterThanOrEqualToConverter"/>.
+        /// </summary>
+        public static IsGreaterThanOrEqualToConverter Default = new IsGreaterThanOrEqualToConverter();
+
+        /// <summary>
+        /// Converts source values to a value for the binding target. The data binding engine calls this method when it propagates the values from source bindings to the binding target.
+        /// </summary>
+        /// <param name="values">The array of values that the source bindings in the <see cref="T:System.Windows.Data.MultiBinding"/> produces. The value <see cref="F:System.Windows.DependencyProperty.UnsetValue"/> indicates that the source binding has no value to provide for conversion.</param>
+        /// <param name="targetType">The type of the binding target property.</param>
+        /// <param name="parameter">The converter parameter to use.</param>
+        /// <param name="culture">The culture to use in the converter.</param>
+        /// <returns>
+        /// A converted value.If the method returns null, the valid null value is used.A return value of <see cref="T:System.Windows.DependencyProperty"/>.<see cref="F:System.Windows.DependencyProperty.UnsetValue"/> indicates that the converter did not produce a value, and that the binding will use the <see cref="P:System.Windows.Data.BindingBase.FallbackValue"/> if it is available, or else will use the default value.A return value of <see cref="T:System.Windows.Data.Binding"/>.<see cref="F:System.Windows.Data.Binding.DoNothing"/> indicates that the binding does not transfer the value or use the <see cref="P:System.Windows.Data.BindingBase.FallbackValue"/> or the default value.
+        /// </returns>
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values != null && values.Length >= 2)
+            {
+                var first = values[0] as IComparable;
+                var second = values[1] as IComparable;
+
+                if (first != null && second != null)
+                {
+                    return first.CompareTo(second) >= 0;
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Converts a binding target value to the source binding values.
+        /// </summary>
+        /// <param name="value">The value that the binding target produces.</param>
+        /// <param name="targetTypes">The array of types to convert to. The array length indicates the number and types of values that are suggested for the method to return.</param>
+        /// <param name="parameter">The converter parameter to use.</param>
+        /// <param name="culture">The culture to use in the converter.</param>
+        /// <returns>
+        /// An array of values that have been converted from the target value back to the source values.
+        /// </returns>
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
