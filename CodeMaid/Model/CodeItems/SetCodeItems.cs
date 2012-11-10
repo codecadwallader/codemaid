a4e251322a7e8e9a77ed117f89fa8a521438cd60
s_cadwallader <steve.cadwallader@gmail.com>
2012-11-10 07:02:34 -0500

    Add an overloaded constructor to SetCodeItems for passing in an existing collection.

--- CodeMaid/Model/CodeItems/SetCodeItems.cs
+++ CodeMaid/Model/CodeItems/SetCodeItems.cs
@@ -20,5 +20,21 @@ namespace SteveCadwallader.CodeMaid.Model.CodeItems
     /// </summary>
     public class SetCodeItems : List<BaseCodeItem>
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SetCodeItems" /> class.
+        /// </summary>
+        public SetCodeItems()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SetCodeItems" /> class initialized with the specified collection members.
+        /// </summary>
+        /// <param name="collection">The collection.</param>
+        public SetCodeItems(IEnumerable<BaseCodeItem> collection)
+            : this()
+        {
+            AddRange(collection);
+        }
     }
 }
