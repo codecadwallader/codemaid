30d322cc88508f339492ac472e131e2a8ffe7661
s_cadwallader <steve.cadwallader@gmail.com>
2013-03-12 04:04:39 -0400

    Update to v0.5.7.1 BETA label.

--- CodeMaid/Properties/AssemblyInfo.cs
+++ CodeMaid/Properties/AssemblyInfo.cs
@@ -14,5 +14,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(false)]
 [assembly: NeutralResourcesLanguage("en-US")]
-[assembly: AssemblyVersion("0.5.7.0")]
-[assembly: AssemblyFileVersion("0.5.7.0")]
+[assembly: AssemblyVersion("0.5.7.1")]
+[assembly: AssemblyFileVersion("0.5.7.1")]
