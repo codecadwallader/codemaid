51882bb62dbb2c6bd2f307707460451cb7a7b42c
Willem Duncan <devnull@localhost>
2013-04-15 11:52:03 +0200

    Added function to get user's language specific indenting settings.

--- CodeMaid/Helpers/CodeCommentHelper.cs
+++ CodeMaid/Helpers/CodeCommentHelper.cs
@@ -19,17 +19,7 @@ namespace SteveCadwallader.CodeMaid.Helpers
     internal static class CodeCommentHelper
     {
         /// <summary>
-        /// Create multi-line Regex pattern based on the comment prefix.
-        /// </summary>
-        /// <param name="prefix"></param>
-        /// <returns></returns>
-        internal static string PrefixToPattern(string prefix)
-        {
-            return String.Format(@"(?<prefix>{0})(?<line>( .*)?)(\r?\n\s*\k<prefix>(?<line>( .*)?))*", prefix);
-        }
-
-        /// <summary>
-        /// Get the comment prefix for the given document's language.
+        /// Get the comment prefix (regex) for the given document's language.
         /// </summary>
         /// <param name="document"></param>
         /// <returns>The comment prefix regex, without trailing spaces.</returns>
@@ -49,6 +39,18 @@ namespace SteveCadwallader.CodeMaid.Helpers
                 default:
                     return null;
             }
+        }
+
+        /// <summary>
+        /// Check if the text is commented-out code.
+        /// </summary>
+        /// <param name="text">The text to check.</param>
+        /// <param name="prefix">The comment prefix.</param>
+        /// <returns><c>true</c> if commented-out code, else <c>false</c>.</returns>
+        internal static bool IsCommentedCode(string text, string prefix)
+        {
+            // Use regex with alternation rather than character class, because otherwise "$" fails.
+            return System.Text.RegularExpressions.Regex.IsMatch(text, String.Format(@"^\s*{0}(?!(\s|\r|\n|$))", prefix));
         }
 
         /// <summary>
@@ -90,15 +92,51 @@ namespace SteveCadwallader.CodeMaid.Helpers
         }
 
         /// <summary>
-        /// Check if the text is commented-out code.
+        /// Create multi-line Regex pattern based on the comment prefix.
         /// </summary>
-        /// <param name="text">The text to check.</param>
-        /// <param name="prefix">The comment prefix.</param>
-        /// <returns><c>true</c> if commented-out code, else <c>false</c>.</returns>
-        internal static bool IsCommentedCode(string text, string prefix)
+        /// <param name="prefix"></param>
+        /// <returns></returns>
+        internal static string PrefixToPattern(string prefix)
         {
-            // Use regex with alternation rather than character class, because otherwise "$" fails.
-            return System.Text.RegularExpressions.Regex.IsMatch(text, String.Format(@"^\s*{0}(?!(\s|\r|\n|$))", prefix));
+            return String.Format(@"(?<prefix>{0})(?<line>( .*)?)(\r?\n\s*\k<prefix>(?<line>( .*)?))*", prefix);
+        }
+
+        /// <summary>
+        /// Get the user's indenting settings.
+        /// </summary>
+        /// <param name="package">Package used to reference DTE.</param>
+        /// <param name="document">Docoument used to get language.</param>
+        /// <returns>An instance of indenting settings for this document.</returns>
+        internal static CodeCommentIndentSettings GetIndentSettings(CodeMaidPackage package, TextDocument document)
+        {
+            return new CodeCommentIndentSettings(package.IDE.Properties["TextEditor", document.Parent.Language]);
+        }
+
+        internal class CodeCommentIndentSettings
+        {
+            public CodeCommentIndentSettings()
+            {
+                IndentStyle = vsIndentStyle.vsIndentStyleSmart;
+                IndentSize = 4;
+                TabSize = 4;
+                InsertTabs = false;
+            }
+
+            public CodeCommentIndentSettings(EnvDTE.Properties settings)
+            {
+                IndentStyle = (vsIndentStyle)settings.Item("IndentStyle").Value;
+                IndentSize = (short)settings.Item("IndentSize").Value;
+                TabSize = (short)settings.Item("TabSize").Value;
+                InsertTabs = (bool)settings.Item("InsertTabs").Value;
+            }
+
+            public short IndentSize { get; private set; }
+
+            public vsIndentStyle IndentStyle { get; private set; }
+
+            public bool InsertTabs { get; private set; }
+
+            public short TabSize { get; private set; }
         }
     }
 }
