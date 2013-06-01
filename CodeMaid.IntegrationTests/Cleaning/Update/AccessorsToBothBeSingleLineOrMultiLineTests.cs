64cf480a4df41f373fc3c9f07a46802e2f1607de
s_cadwallader <steve.cadwallader@gmail.com>
2013-06-01 12:01:58 -0400

    Add unit tests for updating accessors to both be single line or multi line.

---
+++ CodeMaid.IntegrationTests/Cleaning/Update/AccessorsToBothBeSingleLineOrMultiLineTests.cs
@@ -0,0 +1,101 @@
+﻿#region CodeMaid is Copyright 2007-2013 Steve Cadwallader.
+
+// CodeMaid is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License version 3
+// as published by the Free Software Foundation.
+//
+// CodeMaid is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details <http://www.gnu.org/licenses/>.
+
+#endregion CodeMaid is Copyright 2007-2013 Steve Cadwallader.
+
+using System.Linq;
+using EnvDTE;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SteveCadwallader.CodeMaid.Helpers;
+using SteveCadwallader.CodeMaid.IntegrationTests.Helpers;
+using SteveCadwallader.CodeMaid.Logic.Cleaning;
+using SteveCadwallader.CodeMaid.Model.CodeItems;
+using SteveCadwallader.CodeMaid.Properties;
+
+namespace SteveCadwallader.CodeMaid.IntegrationTests.Cleaning.Update
+{
+    [TestClass]
+    [DeploymentItem(@"Cleaning\Update\Data\AccessorsToBothBeSingleLineOrMultiLine.cs", "Data")]
+    [DeploymentItem(@"Cleaning\Update\Data\AccessorsToBothBeSingleLineOrMultiLine_Cleaned.cs", "Data")]
+    public class AccessorsToBothBeSingleLineOrMultiLineTests
+    {
+        #region Setup
+
+        private static UpdateLogic _updateLogic;
+        private ProjectItem _projectItem;
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext testContext)
+        {
+            _updateLogic = UpdateLogic.GetInstance(TestEnvironment.Package);
+            Assert.IsNotNull(_updateLogic);
+        }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _projectItem = TestEnvironment.LoadFileIntoProject(@"Data\AccessorsToBothBeSingleLineOrMultiLine.cs");
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            TestEnvironment.RemoveFromProject(_projectItem);
+        }
+
+        #endregion Setup
+
+        #region Tests
+
+        [TestMethod]
+        [HostType("VS IDE")]
+        public void CleaningUpdateAccessorsToBothBeSingleLineOrMultiLine_CleansAsExpected()
+        {
+            Settings.Default.Cleaning_UpdateAccessorsToBothBeSingleLineOrMultiLine = true;
+
+            CleaningTestHelper.ExecuteCommandAndVerifyResults(RunUpdateAccessorsToBothBeSingleLineOrMultiLine, _projectItem, @"Data\AccessorsToBothBeSingleLineOrMultiLine_Cleaned.cs");
+        }
+
+        [TestMethod]
+        [HostType("VS IDE")]
+        public void CleaningUpdateAccessorsToBothBeSingleLineOrMultiLine_DoesNothingOnSecondPass()
+        {
+            Settings.Default.Cleaning_UpdateAccessorsToBothBeSingleLineOrMultiLine = true;
+
+            CleaningTestHelper.ExecuteCommandTwiceAndVerifyNoChangesOnSecondPass(RunUpdateAccessorsToBothBeSingleLineOrMultiLine, _projectItem);
+        }
+
+        [TestMethod]
+        [HostType("VS IDE")]
+        public void CleaningUpdateAccessorsToBothBeSingleLineOrMultiLine_DoesNothingWhenSettingIsDisabled()
+        {
+            Settings.Default.Cleaning_UpdateAccessorsToBothBeSingleLineOrMultiLine = false;
+
+            CleaningTestHelper.ExecuteCommandAndVerifyNoChanges(RunUpdateAccessorsToBothBeSingleLineOrMultiLine, _projectItem);
+        }
+
+        #endregion Tests
+
+        #region Helpers
+
+        private static void RunUpdateAccessorsToBothBeSingleLineOrMultiLine(Document document)
+        {
+            var codeItems = CodeModelHelper.RetrieveCodeItemsIncludingRegions(document);
+            var events = codeItems.OfType<CodeItemEvent>().ToList();
+            var properties = codeItems.OfType<CodeItemProperty>().ToList();
+
+            _updateLogic.UpdateEventAccessorsToBothBeSingleLineOrMultiLine(events);
+            _updateLogic.UpdatePropertyAccessorsToBothBeSingleLineOrMultiLine(properties);
+        }
+
+        #endregion Helpers
+    }
+}
---
+++ CodeMaid.IntegrationTests/Cleaning/Update/Data/AccessorsToBothBeSingleLineOrMultiLine_Cleaned.cs
@@ -0,0 +1,168 @@
+﻿using System;
+
+namespace SteveCadwallader.CodeMaid.IntegrationTests.Cleaning.Update.Data
+{
+    public class AccessorsToBothBeSingleLineOrMultiLine
+    {
+        private EventHandler _event;
+        private bool _field;
+
+        public event EventHandler SingleLineEvent
+        {
+            add { _event += value; }
+            remove { _event -= value; }
+        }
+
+        public event EventHandler SingleLineAdderSingleLineBodyRemover
+        {
+            add { _event += value; }
+            remove { _event -= value; }
+        }
+
+        public event EventHandler SingleLineAdderMultiLineBodyRemover
+        {
+            add
+            {
+                _event += value;
+            }
+            remove
+            {
+                if (_event != null)
+                {
+                    _event -= value;
+                }
+            }
+        }
+
+        public event EventHandler MultiLineAdderSingleLineRemover
+        {
+            add
+            {
+                if (_event == null)
+                {
+                    _event += value;
+                }
+            }
+            remove
+            {
+                _event -= value;
+            }
+        }
+
+        public event EventHandler MultiLineAdderSingleLineBodyRemover
+        {
+            add
+            {
+                if (_event == null)
+                {
+                    _event += value;
+                }
+            }
+            remove
+            {
+                _event -= value;
+            }
+        }
+
+        public event EventHandler MultiLineAdderMultiLineRemover
+        {
+            add
+            {
+                if (_event == null)
+                {
+                    _event += value;
+                }
+            }
+            remove
+            {
+                if (_event != null)
+                {
+                    _event -= value;
+                }
+            }
+        }
+
+        public bool AutoImplementedProperty { get; set; }
+
+        public bool SingleLineProperty
+        {
+            get { return _field; }
+            set { _field = value; }
+        }
+
+        public bool SingleLineGetterSingleLineBodySetter
+        {
+            get { return _field; }
+            set { _field = value; }
+        }
+
+        public bool SingleLineGetterMultiLineBodySetter
+        {
+            get
+            {
+                return _field;
+            }
+            set
+            {
+                if (_field != value)
+                {
+                    _field = value;
+                }
+            }
+        }
+
+        public bool MultiLineGetterSingleLineSetter
+        {
+            get
+            {
+                if (_field == false)
+                {
+                    _field = true;
+                }
+
+                return _field;
+            }
+            set
+            {
+                _field = value;
+            }
+        }
+
+        public bool MultiLineGetterSingleLineBodySetter
+        {
+            get
+            {
+                if (_field == false)
+                {
+                    _field = true;
+                }
+
+                return _field;
+            }
+            set
+            {
+                _field = value;
+            }
+        }
+
+        public bool MultiLineGetterMultiLineSetter
+        {
+            get
+            {
+                if (_field == false)
+                {
+                    _field = true;
+                }
+
+                return _field;
+            }
+            set
+            {
+                if (_field != value)
+                {
+                    _field = value;
+                }
+            }
+        }
+    }
+}
