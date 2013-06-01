84dae8a4445c49ea70574933353b72eaaa7adbe0
s_cadwallader <steve.cadwallader@gmail.com>
2013-06-01 10:38:57 -0400

    Add unit tests for remove blank lines after attributes.

---
+++ CodeMaid.IntegrationTests/Cleaning/Remove/BlankLinesAfterAttributesTests.cs
@@ -0,0 +1,95 @@
+﻿#region CodeMaid is Copyright 2007-2013 Steve Cadwallader.
+
+// CodeMaid is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License version 3
+// as published by the Free Software Foundation.
+//
+// CodeMaid is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details <http://www.gnu.org/licenses/>.
+
+#endregion CodeMaid is Copyright 2007-2013 Steve Cadwallader.
+
+using EnvDTE;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SteveCadwallader.CodeMaid.IntegrationTests.Helpers;
+using SteveCadwallader.CodeMaid.Logic.Cleaning;
+using SteveCadwallader.CodeMaid.Properties;
+
+namespace SteveCadwallader.CodeMaid.IntegrationTests.Cleaning.Remove
+{
+    [TestClass]
+    [DeploymentItem(@"Cleaning\Remove\Data\BlankLinesAfterAttributes.cs", "Data")]
+    [DeploymentItem(@"Cleaning\Remove\Data\BlankLinesAfterAttributes_Cleaned.cs", "Data")]
+    public class BlankLinesAfterAttributesTests
+    {
+        #region Setup
+
+        private static RemoveWhitespaceLogic _removeWhitespaceLogic;
+        private ProjectItem _projectItem;
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext testContext)
+        {
+            _removeWhitespaceLogic = RemoveWhitespaceLogic.GetInstance(TestEnvironment.Package);
+            Assert.IsNotNull(_removeWhitespaceLogic);
+        }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _projectItem = TestEnvironment.LoadFileIntoProject(@"Data\BlankLinesAfterAttributes.cs");
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            TestEnvironment.RemoveFromProject(_projectItem);
+        }
+
+        #endregion Setup
+
+        #region Tests
+
+        [TestMethod]
+        [HostType("VS IDE")]
+        public void CleaningRemoveBlankLinesAfterAttributes_CleansAsExpected()
+        {
+            Settings.Default.Cleaning_RemoveBlankLinesAfterAttributes = true;
+
+            CleaningTestHelper.ExecuteCommandAndVerifyResults(RunRemoveBlankLinesAfterAttributes, _projectItem, @"Data\BlankLinesAfterAttributes_Cleaned.cs");
+        }
+
+        [TestMethod]
+        [HostType("VS IDE")]
+        public void CleaningRemoveBlankLinesAfterAttributes_DoesNothingOnSecondPass()
+        {
+            Settings.Default.Cleaning_RemoveBlankLinesAfterAttributes = true;
+
+            CleaningTestHelper.ExecuteCommandTwiceAndVerifyNoChangesOnSecondPass(RunRemoveBlankLinesAfterAttributes, _projectItem);
+        }
+
+        [TestMethod]
+        [HostType("VS IDE")]
+        public void CleaningRemoveBlankLinesAfterAttributes_DoesNothingWhenSettingIsDisabled()
+        {
+            Settings.Default.Cleaning_RemoveBlankLinesAfterAttributes = false;
+
+            CleaningTestHelper.ExecuteCommandAndVerifyNoChanges(RunRemoveBlankLinesAfterAttributes, _projectItem);
+        }
+
+        #endregion Tests
+
+        #region Helpers
+
+        private static void RunRemoveBlankLinesAfterAttributes(Document document)
+        {
+            var textDocument = TestUtils.GetTextDocument(document);
+
+            _removeWhitespaceLogic.RemoveBlankLinesAfterAttributes(textDocument);
+        }
+
+        #endregion Helpers
+    }
+}
---
+++ CodeMaid.IntegrationTests/Cleaning/Remove/Data/BlankLinesAfterAttributes_Cleaned.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+
+namespace SteveCadwallader.CodeMaid.IntegrationTests.Cleaning.Remove.Data
+{
+    [Description("Some description")]
+    public class BlankLinesAfterAttributes
+    {
+        public void NoAttributeMethod()
+        {
+        }
+
+        [Description("Some description")]
+        public void SingleAttributeMethod()
+        {
+        }
+
+        [Description("Some description")]
+        [Category("Some category")]
+        public void MultipleAttributeMethod()
+        {
+        }
+    }
+}
