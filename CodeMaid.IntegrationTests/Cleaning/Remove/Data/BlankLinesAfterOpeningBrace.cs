4ee5600f37ba0f54edced495feb5eca68790d3c6
s_cadwallader <steve.cadwallader@gmail.com>
2013-06-01 10:53:20 -0400

    Add unit tests for remove blank lines after opening brace.

---
+++ CodeMaid.IntegrationTests/Cleaning/Remove/Data/BlankLinesAfterOpeningBrace.cs
@@ -0,0 +1,23 @@
+﻿namespace SteveCadwallader.CodeMaid.IntegrationTests.Cleaning.Remove.Data
+{
+    public class BlankLinesAfterOpeningBrace
+    {
+
+
+        public void Method()
+        {
+
+            if (true)
+            { // Trailing comments should be kept in place.
+
+            }
+            else
+            {
+
+
+
+
+            }
+        }
+    }
+}
---
+++ CodeMaid.IntegrationTests/Cleaning/Remove/Data/BlankLinesAfterOpeningBrace_Cleaned.cs
@@ -0,0 +1,15 @@
+﻿namespace SteveCadwallader.CodeMaid.IntegrationTests.Cleaning.Remove.Data
+{
+    public class BlankLinesAfterOpeningBrace
+    {
+        public void Method()
+        {
+            if (true)
+            { // Trailing comments should be kept in place.
+            }
+            else
+            {
+            }
+        }
+    }
+}
