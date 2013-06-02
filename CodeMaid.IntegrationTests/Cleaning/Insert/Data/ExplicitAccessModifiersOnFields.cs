9480004de763f3d7716eb8132ccfc59a6e7139c0
s_cadwallader <steve.cadwallader@gmail.com>
2013-06-02 07:56:22 -0400

    Extend insert explicit access modifiers on fields unit tests to verify enumeration items which are registered as fields are ignored.

--- CodeMaid.IntegrationTests/Cleaning/Insert/Data/ExplicitAccessModifiersOnFields.cs
+++ CodeMaid.IntegrationTests/Cleaning/Insert/Data/ExplicitAccessModifiersOnFields.cs
@@ -8,4 +8,12 @@
         protected bool ProtectedField;
         private bool PrivateField;
     }
+
+    // Enumeration items are registered as fields, but should not be given an explicit access modifier.
+    public enum ExplicitAccessModifiersOnFieldsEnum
+    {
+        EnumItem1,
+        EnumItem2,
+        EnumItem3
+    }
 }
