270295a614b675eaa4fb593eaec9ebe830e4ca67
s_cadwallader <steve.cadwallader@gmail.com>
2013-06-02 08:43:40 -0400

    Add unit tests for insert blank line padding after region tags.

---
+++ CodeMaid.IntegrationTests/Cleaning/Insert/BlankLinePaddingAfterRegionTagsTests.cs
@@ -0,0 +1,100 @@
+﻿#region CodeMaid is Copyright 2007-2013 Steve Cadwallader.
+
+// CodeMaid is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License version 3
+// as published by the Free Software Foundation.
+//
+// CodeMaid is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details <http://www.gnu.org/licenses/>.
+
+#endregion CodeMaid is Copyright 2007-2013 Steve Cadwallader.
+
+using System.Linq;
+using EnvDTE;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SteveCadwallader.CodeMaid.Helpers;
+using SteveCadwallader.CodeMaid.IntegrationTests.Helpers;
+using SteveCadwallader.CodeMaid.Logic.Cleaning;
+using SteveCadwallader.CodeMaid.Model.CodeItems;
+using SteveCadwallader.CodeMaid.Properties;
+
+namespace SteveCadwallader.CodeMaid.IntegrationTests.Cleaning.Insert
+{
+    [TestClass]
+    [DeploymentItem(@"Cleaning\Insert\Data\BlankLinePaddingAfterRegionTags.cs", "Data")]
+    [DeploymentItem(@"Cleaning\Insert\Data\BlankLinePaddingAfterRegionTags_Cleaned.cs", "Data")]
+    public class BlankLinePaddingAfterRegionTagsTests
+    {
+        #region Setup
+
+        private static InsertBlankLinePaddingLogic _insertBlankLinePaddingLogic;
+        private ProjectItem _projectItem;
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext testContext)
+        {
+            _insertBlankLinePaddingLogic = InsertBlankLinePaddingLogic.GetInstance(TestEnvironment.Package);
+            Assert.IsNotNull(_insertBlankLinePaddingLogic);
+        }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            TestEnvironment.CommonTestInitialize();
+            _projectItem = TestEnvironment.LoadFileIntoProject(@"Data\BlankLinePaddingAfterRegionTags.cs");
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            TestEnvironment.RemoveFromProject(_projectItem);
+        }
+
+        #endregion Setup
+
+        #region Tests
+
+        [TestMethod]
+        [HostType("VS IDE")]
+        public void CleaningInsertBlankLinePaddingAfterRegionTags_CleansAsExpected()
+        {
+            Settings.Default.Cleaning_InsertBlankLinePaddingAfterRegionTags = true;
+
+            CleaningTestHelper.ExecuteCommandAndVerifyResults(RunInsertBlankLinePaddingAfterRegionTags, _projectItem, @"Data\BlankLinePaddingAfterRegionTags_Cleaned.cs");
+        }
+
+        [TestMethod]
+        [HostType("VS IDE")]
+        public void CleaningInsertBlankLinePaddingAfterRegionTags_DoesNothingOnSecondPass()
+        {
+            Settings.Default.Cleaning_InsertBlankLinePaddingAfterRegionTags = true;
+
+            CleaningTestHelper.ExecuteCommandTwiceAndVerifyNoChangesOnSecondPass(RunInsertBlankLinePaddingAfterRegionTags, _projectItem);
+        }
+
+        [TestMethod]
+        [HostType("VS IDE")]
+        public void CleaningInsertBlankLinePaddingAfterRegionTags_DoesNothingWhenSettingIsDisabled()
+        {
+            Settings.Default.Cleaning_InsertBlankLinePaddingAfterRegionTags = false;
+
+            CleaningTestHelper.ExecuteCommandAndVerifyNoChanges(RunInsertBlankLinePaddingAfterRegionTags, _projectItem);
+        }
+
+        #endregion Tests
+
+        #region Helpers
+
+        private static void RunInsertBlankLinePaddingAfterRegionTags(Document document)
+        {
+            var codeItems = CodeModelHelper.RetrieveCodeItemsIncludingRegions(document);
+            var regions = codeItems.OfType<CodeItemRegion>().ToList();
+
+            _insertBlankLinePaddingLogic.InsertPaddingAfterRegionTags(regions);
+        }
+
+        #endregion Helpers
+    }
+}
