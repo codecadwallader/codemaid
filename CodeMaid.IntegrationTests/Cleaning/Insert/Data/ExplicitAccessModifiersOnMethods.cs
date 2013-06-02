c6589bd4a03ed12a902b1b6ab26e9431b29b98f9
s_cadwallader <steve.cadwallader@gmail.com>
2013-06-02 07:29:39 -0400

    Add unit tests for insert explicit access modifiers on methods.

---
+++ CodeMaid.IntegrationTests/Cleaning/Insert/Data/ExplicitAccessModifiersOnMethods.cs
@@ -0,0 +1,25 @@
+﻿namespace SteveCadwallader.CodeMaid.IntegrationTests.Cleaning.Insert.Data
+{
+    public class ExplicitAccessModifiersOnMethods
+    {
+        void UnspecifiedMethod()
+        {
+        }
+
+        public void PublicMethod()
+        {
+        }
+
+        internal void InternalMethod()
+        {
+        }
+
+        protected void ProtectedMethod()
+        {
+        }
+
+        private void PrivateMethod()
+        {
+        }
+    }
+}
---
+++ CodeMaid.IntegrationTests/Cleaning/Insert/ExplicitAccessModifiersOnMethodsTests.cs
@@ -0,0 +1,100 @@
+﻿#region CodeMaid is Copyright 2007-2013 Steve Cadwallader.
+
+// CodeMaid is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License version 3
+// as published by the Free Software Foundation.
+//
+// CodeMaid is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details <http://www.gnu.org/licenses/>.
+
+#endregion CodeMaid is Copyright 2007-2013 Steve Cadwallader.
+
+using System.Linq;
+using EnvDTE;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SteveCadwallader.CodeMaid.Helpers;
+using SteveCadwallader.CodeMaid.IntegrationTests.Helpers;
+using SteveCadwallader.CodeMaid.Logic.Cleaning;
+using SteveCadwallader.CodeMaid.Model.CodeItems;
+using SteveCadwallader.CodeMaid.Properties;
+
+namespace SteveCadwallader.CodeMaid.IntegrationTests.Cleaning.Insert
+{
+    [TestClass]
+    [DeploymentItem(@"Cleaning\Insert\Data\ExplicitAccessModifiersOnMethods.cs", "Data")]
+    [DeploymentItem(@"Cleaning\Insert\Data\ExplicitAccessModifiersOnMethods_Cleaned.cs", "Data")]
+    public class ExplicitAccessModifiersOnMethodsTests
+    {
+        #region Setup
+
+        private static InsertExplicitAccessModifierLogic _insertExplicitAccessModifierLogic;
+        private ProjectItem _projectItem;
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext testContext)
+        {
+            _insertExplicitAccessModifierLogic = InsertExplicitAccessModifierLogic.GetInstance();
+            Assert.IsNotNull(_insertExplicitAccessModifierLogic);
+        }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            TestEnvironment.CommonTestInitialize();
+            _projectItem = TestEnvironment.LoadFileIntoProject(@"Data\ExplicitAccessModifiersOnMethods.cs");
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            TestEnvironment.RemoveFromProject(_projectItem);
+        }
+
+        #endregion Setup
+
+        #region Tests
+
+        [TestMethod]
+        [HostType("VS IDE")]
+        public void CleaningInsertExplicitAccessModifiersOnMethods_CleansAsExpected()
+        {
+            Settings.Default.Cleaning_InsertExplicitAccessModifiersOnMethods = true;
+
+            CleaningTestHelper.ExecuteCommandAndVerifyResults(RunInsertExplicitAccessModifiersOnMethods, _projectItem, @"Data\ExplicitAccessModifiersOnMethods_Cleaned.cs");
+        }
+
+        [TestMethod]
+        [HostType("VS IDE")]
+        public void CleaningInsertExplicitAccessModifiersOnMethods_DoesNothingOnSecondPass()
+        {
+            Settings.Default.Cleaning_InsertExplicitAccessModifiersOnMethods = true;
+
+            CleaningTestHelper.ExecuteCommandTwiceAndVerifyNoChangesOnSecondPass(RunInsertExplicitAccessModifiersOnMethods, _projectItem);
+        }
+
+        [TestMethod]
+        [HostType("VS IDE")]
+        public void CleaningInsertExplicitAccessModifiersOnMethods_DoesNothingWhenSettingIsDisabled()
+        {
+            Settings.Default.Cleaning_InsertExplicitAccessModifiersOnMethods = false;
+
+            CleaningTestHelper.ExecuteCommandAndVerifyNoChanges(RunInsertExplicitAccessModifiersOnMethods, _projectItem);
+        }
+
+        #endregion Tests
+
+        #region Helpers
+
+        private static void RunInsertExplicitAccessModifiersOnMethods(Document document)
+        {
+            var codeItems = CodeModelHelper.RetrieveCodeItemsIncludingRegions(document);
+            var methods = codeItems.OfType<CodeItemMethod>().ToList();
+
+            _insertExplicitAccessModifierLogic.InsertExplicitAccessModifiersOnMethods(methods);
+        }
+
+        #endregion Helpers
+    }
+}
