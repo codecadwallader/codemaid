00daf8175efa1fd51bfa028af91658774db0763f
s_cadwallader <steve.cadwallader@gmail.com>
2013-06-02 06:56:53 -0400

    Add unit tests for insert explicit access modifiers on classes.

---
+++ CodeMaid.IntegrationTests/Cleaning/Insert/Data/ExplicitAccessModifiersOnClasses.cs
@@ -0,0 +1,25 @@
+﻿namespace SteveCadwallader.CodeMaid.IntegrationTests.Cleaning.Insert.Data
+{
+    class ExplicitAccessModifiersOnClasses
+    {
+    }
+
+    public class PublicClass
+    {
+        class NestedClass
+        {
+        }
+
+        protected class ProtectedClass
+        {
+        }
+
+        private class PrivateClass
+        {
+        }
+    }
+
+    internal class InternalClass
+    {
+    }
+}
---
+++ CodeMaid.IntegrationTests/Cleaning/Insert/Data/ExplicitAccessModifiersOnClasses_Cleaned.cs
@@ -0,0 +1,25 @@
+﻿namespace SteveCadwallader.CodeMaid.IntegrationTests.Cleaning.Insert.Data
+{
+    internal class ExplicitAccessModifiersOnClasses
+    {
+    }
+
+    public class PublicClass
+    {
+        private class NestedClass
+        {
+        }
+
+        protected class ProtectedClass
+        {
+        }
+
+        private class PrivateClass
+        {
+        }
+    }
+
+    internal class InternalClass
+    {
+    }
+}
