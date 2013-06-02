553d4a7decce624e6eb77e26a159565e9df8dd8e
s_cadwallader <steve.cadwallader@gmail.com>
2013-06-02 09:04:45 -0400

    Add unit tests for insert blank line padding before case statements.

---
+++ CodeMaid.IntegrationTests/Cleaning/Insert/BlankLinePaddingBeforeCaseStatementsTests.cs
@@ -0,0 +1,96 @@
+﻿#region CodeMaid is Copyright 2007-2013 Steve Cadwallader.
+
+// CodeMaid is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License version 3
+// as published by the Free Software Foundation.
+//
+// CodeMaid is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details <http://www.gnu.org/licenses/>.
+
+#endregion CodeMaid is Copyright 2007-2013 Steve Cadwallader.
+
+using EnvDTE;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SteveCadwallader.CodeMaid.IntegrationTests.Helpers;
+using SteveCadwallader.CodeMaid.Logic.Cleaning;
+using SteveCadwallader.CodeMaid.Properties;
+
+namespace SteveCadwallader.CodeMaid.IntegrationTests.Cleaning.Insert
+{
+    [TestClass]
+    [DeploymentItem(@"Cleaning\Insert\Data\BlankLinePaddingBeforeCaseStatements.cs", "Data")]
+    [DeploymentItem(@"Cleaning\Insert\Data\BlankLinePaddingBeforeCaseStatements_Cleaned.cs", "Data")]
+    public class BlankLinePaddingBeforeCaseStatementsTests
+    {
+        #region Setup
+
+        private static InsertBlankLinePaddingLogic _insertBlankLinePaddingLogic;
+        private ProjectItem _projectItem;
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext testContext)
+        {
+            _insertBlankLinePaddingLogic = InsertBlankLinePaddingLogic.GetInstance(TestEnvironment.Package);
+            Assert.IsNotNull(_insertBlankLinePaddingLogic);
+        }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            TestEnvironment.CommonTestInitialize();
+            _projectItem = TestEnvironment.LoadFileIntoProject(@"Data\BlankLinePaddingBeforeCaseStatements.cs");
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            TestEnvironment.RemoveFromProject(_projectItem);
+        }
+
+        #endregion Setup
+
+        #region Tests
+
+        [TestMethod]
+        [HostType("VS IDE")]
+        public void CleaningInsertBlankLinePaddingBeforeCaseStatements_CleansAsExpected()
+        {
+            Settings.Default.Cleaning_InsertBlankLinePaddingBeforeCaseStatements = true;
+
+            CleaningTestHelper.ExecuteCommandAndVerifyResults(RunInsertBlankLinePaddingBeforeCaseStatements, _projectItem, @"Data\BlankLinePaddingBeforeCaseStatements_Cleaned.cs");
+        }
+
+        [TestMethod]
+        [HostType("VS IDE")]
+        public void CleaningInsertBlankLinePaddingBeforeCaseStatements_DoesNothingOnSecondPass()
+        {
+            Settings.Default.Cleaning_InsertBlankLinePaddingBeforeCaseStatements = true;
+
+            CleaningTestHelper.ExecuteCommandTwiceAndVerifyNoChangesOnSecondPass(RunInsertBlankLinePaddingBeforeCaseStatements, _projectItem);
+        }
+
+        [TestMethod]
+        [HostType("VS IDE")]
+        public void CleaningInsertBlankLinePaddingBeforeCaseStatements_DoesNothingWhenSettingIsDisabled()
+        {
+            Settings.Default.Cleaning_InsertBlankLinePaddingBeforeCaseStatements = false;
+
+            CleaningTestHelper.ExecuteCommandAndVerifyNoChanges(RunInsertBlankLinePaddingBeforeCaseStatements, _projectItem);
+        }
+
+        #endregion Tests
+
+        #region Helpers
+
+        private static void RunInsertBlankLinePaddingBeforeCaseStatements(Document document)
+        {
+            var textDocument = TestUtils.GetTextDocument(document);
+
+            _insertBlankLinePaddingLogic.InsertPaddingBeforeCaseStatements(textDocument);
+        }
+
+        #endregion Helpers
+    }
+}
---
+++ CodeMaid.IntegrationTests/Cleaning/Insert/Data/BlankLinePaddingBeforeCaseStatements_Cleaned.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace SteveCadwallader.CodeMaid.IntegrationTests.Cleaning.Insert.Data
+{
+    public class BlankLinePaddingBeforeCaseStatements
+    {
+        public void VoidReturnTest()
+        {
+            switch (DateTime.Today.DayOfWeek)
+            {
+                case DayOfWeek.Monday:
+                    break;
+
+                case DayOfWeek.Tuesday:
+                case DayOfWeek.Wednesday:
+                    return;
+
+                case DayOfWeek.Thursday: break;
+                case DayOfWeek.Friday: return;
+                case DayOfWeek.Saturday:
+                case DayOfWeek.Sunday:
+                    return;
+
+                default:
+                    return;
+            }
+        }
+
+        public int IntReturnTest()
+        {
+            switch (DateTime.Today.DayOfWeek)
+            {
+                case DayOfWeek.Monday:
+                    return 0;
+
+                case DayOfWeek.Tuesday:
+                case DayOfWeek.Wednesday:
+                    return
+                        0 + 1;
+                case DayOfWeek.Thursday: break;
+                case DayOfWeek.Friday: return 2;
+                case DayOfWeek.Saturday:
+                    return 3;
+
+                default:
+                    return 4;
+            }
+
+            return 0;
+        }
+    }
+}
