f6908dba13bbae61d2d3196ef58b670f901325e7
s_cadwallader <steve.cadwallader@gmail.com>
2013-06-02 07:13:36 -0400

    Add unit tests for insert explicit access modifiers on enumerations.

---
+++ CodeMaid.IntegrationTests/Cleaning/Insert/Data/ExplicitAccessModifiersOnEnumerations.cs
@@ -0,0 +1,45 @@
+﻿namespace SteveCadwallader.CodeMaid.IntegrationTests.Cleaning.Insert.Data
+{
+    enum UnspecifiedEnum
+    {
+    }
+
+    public enum PublicEnum
+    {
+    }
+
+    internal enum InternalEnum
+    {
+    }
+
+    protected enum ProtectedEnum
+    {
+    }
+
+    private enum PrivateEnum
+    {
+    }
+
+    public class ExplicitAccessModifiersOnEnumerations
+    {
+        enum UnspecifiedEnum
+        {
+        }
+
+        public enum PublicEnum
+        {
+        }
+
+        internal enum InternalEnum
+        {
+        }
+
+        protected enum ProtectedEnum
+        {
+        }
+
+        private enum PrivateEnum
+        {
+        }
+    }
+}
---
+++ CodeMaid.IntegrationTests/Cleaning/Insert/ExplicitAccessModifiersOnEnumerationsTests.cs
@@ -0,0 +1,100 @@
+﻿#region CodeMaid is Copyright 2007-2013 Steve Cadwallader.
+
+// CodeMaid is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License version 3
+// as published by the Free Software Foundation.
+//
+// CodeMaid is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details <http://www.gnu.org/licenses/>.
+
+#endregion CodeMaid is Copyright 2007-2013 Steve Cadwallader.
+
+using System.Linq;
+using EnvDTE;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SteveCadwallader.CodeMaid.Helpers;
+using SteveCadwallader.CodeMaid.IntegrationTests.Helpers;
+using SteveCadwallader.CodeMaid.Logic.Cleaning;
+using SteveCadwallader.CodeMaid.Model.CodeItems;
+using SteveCadwallader.CodeMaid.Properties;
+
+namespace SteveCadwallader.CodeMaid.IntegrationTests.Cleaning.Insert
+{
+    [TestClass]
+    [DeploymentItem(@"Cleaning\Insert\Data\ExplicitAccessModifiersOnEnumerations.cs", "Data")]
+    [DeploymentItem(@"Cleaning\Insert\Data\ExplicitAccessModifiersOnEnumerations_Cleaned.cs", "Data")]
+    public class ExplicitAccessModifiersOnEnumerationsTests
+    {
+        #region Setup
+
+        private static InsertExplicitAccessModifierLogic _insertExplicitAccessModifierLogic;
+        private ProjectItem _projectItem;
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext testContext)
+        {
+            _insertExplicitAccessModifierLogic = InsertExplicitAccessModifierLogic.GetInstance();
+            Assert.IsNotNull(_insertExplicitAccessModifierLogic);
+        }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            TestEnvironment.CommonTestInitialize();
+            _projectItem = TestEnvironment.LoadFileIntoProject(@"Data\ExplicitAccessModifiersOnEnumerations.cs");
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            TestEnvironment.RemoveFromProject(_projectItem);
+        }
+
+        #endregion Setup
+
+        #region Tests
+
+        [TestMethod]
+        [HostType("VS IDE")]
+        public void CleaningInsertExplicitAccessModifiersOnEnumerations_CleansAsExpected()
+        {
+            Settings.Default.Cleaning_InsertExplicitAccessModifiersOnEnumerations = true;
+
+            CleaningTestHelper.ExecuteCommandAndVerifyResults(RunInsertExplicitAccessModifiersOnEnumerations, _projectItem, @"Data\ExplicitAccessModifiersOnEnumerations_Cleaned.cs");
+        }
+
+        [TestMethod]
+        [HostType("VS IDE")]
+        public void CleaningInsertExplicitAccessModifiersOnEnumerations_DoesNothingOnSecondPass()
+        {
+            Settings.Default.Cleaning_InsertExplicitAccessModifiersOnEnumerations = true;
+
+            CleaningTestHelper.ExecuteCommandTwiceAndVerifyNoChangesOnSecondPass(RunInsertExplicitAccessModifiersOnEnumerations, _projectItem);
+        }
+
+        [TestMethod]
+        [HostType("VS IDE")]
+        public void CleaningInsertExplicitAccessModifiersOnEnumerations_DoesNothingWhenSettingIsDisabled()
+        {
+            Settings.Default.Cleaning_InsertExplicitAccessModifiersOnEnumerations = false;
+
+            CleaningTestHelper.ExecuteCommandAndVerifyNoChanges(RunInsertExplicitAccessModifiersOnEnumerations, _projectItem);
+        }
+
+        #endregion Tests
+
+        #region Helpers
+
+        private static void RunInsertExplicitAccessModifiersOnEnumerations(Document document)
+        {
+            var codeItems = CodeModelHelper.RetrieveCodeItemsIncludingRegions(document);
+            var enumerations = codeItems.OfType<CodeItemEnum>().ToList();
+
+            _insertExplicitAccessModifierLogic.InsertExplicitAccessModifiersOnEnumerations(enumerations);
+        }
+
+        #endregion Helpers
+    }
+}
