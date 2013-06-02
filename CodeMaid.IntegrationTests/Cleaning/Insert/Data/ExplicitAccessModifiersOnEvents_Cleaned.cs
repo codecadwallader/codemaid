1bd2bf4d1fe0e3904ad4efef52ada152da8f968b
s_cadwallader <steve.cadwallader@gmail.com>
2013-06-02 07:17:21 -0400

    Add unit tests for insert explicit access modifiers on events.

---
+++ CodeMaid.IntegrationTests/Cleaning/Insert/Data/ExplicitAccessModifiersOnEvents_Cleaned.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SteveCadwallader.CodeMaid.IntegrationTests.Cleaning.Insert.Data
+{
+    public class ExplicitAccessModifiersOnEvents
+    {
+        private event EventHandler UnspecifiedEvent;
+
+        public event EventHandler PublicEvent;
+
+        internal event EventHandler InternalEvent;
+
+        protected event EventHandler ProtectedEvent;
+
+        private event EventHandler PrivateEvent;
+    }
+}
---
+++ CodeMaid.IntegrationTests/Cleaning/Insert/ExplicitAccessModifiersOnEventsTests.cs
@@ -0,0 +1,100 @@
+﻿#region CodeMaid is Copyright 2007-2013 Steve Cadwallader.
+
+// CodeMaid is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License version 3
+// as published by the Free Software Foundation.
+//
+// CodeMaid is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details <http://www.gnu.org/licenses/>.
+
+#endregion CodeMaid is Copyright 2007-2013 Steve Cadwallader.
+
+using System.Linq;
+using EnvDTE;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SteveCadwallader.CodeMaid.Helpers;
+using SteveCadwallader.CodeMaid.IntegrationTests.Helpers;
+using SteveCadwallader.CodeMaid.Logic.Cleaning;
+using SteveCadwallader.CodeMaid.Model.CodeItems;
+using SteveCadwallader.CodeMaid.Properties;
+
+namespace SteveCadwallader.CodeMaid.IntegrationTests.Cleaning.Insert
+{
+    [TestClass]
+    [DeploymentItem(@"Cleaning\Insert\Data\ExplicitAccessModifiersOnEvents.cs", "Data")]
+    [DeploymentItem(@"Cleaning\Insert\Data\ExplicitAccessModifiersOnEvents_Cleaned.cs", "Data")]
+    public class ExplicitAccessModifiersOnEventsTests
+    {
+        #region Setup
+
+        private static InsertExplicitAccessModifierLogic _insertExplicitAccessModifierLogic;
+        private ProjectItem _projectItem;
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext testContext)
+        {
+            _insertExplicitAccessModifierLogic = InsertExplicitAccessModifierLogic.GetInstance();
+            Assert.IsNotNull(_insertExplicitAccessModifierLogic);
+        }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            TestEnvironment.CommonTestInitialize();
+            _projectItem = TestEnvironment.LoadFileIntoProject(@"Data\ExplicitAccessModifiersOnEvents.cs");
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            TestEnvironment.RemoveFromProject(_projectItem);
+        }
+
+        #endregion Setup
+
+        #region Tests
+
+        [TestMethod]
+        [HostType("VS IDE")]
+        public void CleaningInsertExplicitAccessModifiersOnEvents_CleansAsExpected()
+        {
+            Settings.Default.Cleaning_InsertExplicitAccessModifiersOnEvents = true;
+
+            CleaningTestHelper.ExecuteCommandAndVerifyResults(RunInsertExplicitAccessModifiersOnEvents, _projectItem, @"Data\ExplicitAccessModifiersOnEvents_Cleaned.cs");
+        }
+
+        [TestMethod]
+        [HostType("VS IDE")]
+        public void CleaningInsertExplicitAccessModifiersOnEvents_DoesNothingOnSecondPass()
+        {
+            Settings.Default.Cleaning_InsertExplicitAccessModifiersOnEvents = true;
+
+            CleaningTestHelper.ExecuteCommandTwiceAndVerifyNoChangesOnSecondPass(RunInsertExplicitAccessModifiersOnEvents, _projectItem);
+        }
+
+        [TestMethod]
+        [HostType("VS IDE")]
+        public void CleaningInsertExplicitAccessModifiersOnEvents_DoesNothingWhenSettingIsDisabled()
+        {
+            Settings.Default.Cleaning_InsertExplicitAccessModifiersOnEvents = false;
+
+            CleaningTestHelper.ExecuteCommandAndVerifyNoChanges(RunInsertExplicitAccessModifiersOnEvents, _projectItem);
+        }
+
+        #endregion Tests
+
+        #region Helpers
+
+        private static void RunInsertExplicitAccessModifiersOnEvents(Document document)
+        {
+            var codeItems = CodeModelHelper.RetrieveCodeItemsIncludingRegions(document);
+            var events = codeItems.OfType<CodeItemEvent>().ToList();
+
+            _insertExplicitAccessModifierLogic.InsertExplicitAccessModifiersOnEvents(events);
+        }
+
+        #endregion Helpers
+    }
+}
