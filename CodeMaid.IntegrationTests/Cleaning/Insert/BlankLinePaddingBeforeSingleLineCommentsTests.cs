05f5c219865273a6a268580e80d8fda1f288c090
s_cadwallader <steve.cadwallader@gmail.com>
2013-06-02 09:11:31 -0400

    Add unit tests for inserting blank line padding before single line comments.

---
+++ CodeMaid.IntegrationTests/Cleaning/Insert/BlankLinePaddingBeforeSingleLineCommentsTests.cs
@@ -0,0 +1,96 @@
+﻿#region CodeMaid is Copyright 2007-2013 Steve Cadwallader.
+
+// CodeMaid is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License version 3
+// as published by the Free Software Foundation.
+//
+// CodeMaid is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details <http://www.gnu.org/licenses/>.
+
+#endregion CodeMaid is Copyright 2007-2013 Steve Cadwallader.
+
+using EnvDTE;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SteveCadwallader.CodeMaid.IntegrationTests.Helpers;
+using SteveCadwallader.CodeMaid.Logic.Cleaning;
+using SteveCadwallader.CodeMaid.Properties;
+
+namespace SteveCadwallader.CodeMaid.IntegrationTests.Cleaning.Insert
+{
+    [TestClass]
+    [DeploymentItem(@"Cleaning\Insert\Data\BlankLinePaddingBeforeSingleLineComments.cs", "Data")]
+    [DeploymentItem(@"Cleaning\Insert\Data\BlankLinePaddingBeforeSingleLineComments_Cleaned.cs", "Data")]
+    public class BlankLinePaddingBeforeSingleLineCommentsTests
+    {
+        #region Setup
+
+        private static InsertBlankLinePaddingLogic _insertBlankLinePaddingLogic;
+        private ProjectItem _projectItem;
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext testContext)
+        {
+            _insertBlankLinePaddingLogic = InsertBlankLinePaddingLogic.GetInstance(TestEnvironment.Package);
+            Assert.IsNotNull(_insertBlankLinePaddingLogic);
+        }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            TestEnvironment.CommonTestInitialize();
+            _projectItem = TestEnvironment.LoadFileIntoProject(@"Data\BlankLinePaddingBeforeSingleLineComments.cs");
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            TestEnvironment.RemoveFromProject(_projectItem);
+        }
+
+        #endregion Setup
+
+        #region Tests
+
+        [TestMethod]
+        [HostType("VS IDE")]
+        public void CleaningInsertBlankLinePaddingBeforeSingleLineComments_CleansAsExpected()
+        {
+            Settings.Default.Cleaning_InsertBlankLinePaddingBeforeSingleLineComments = true;
+
+            CleaningTestHelper.ExecuteCommandAndVerifyResults(RunInsertBlankLinePaddingBeforeSingleLineComments, _projectItem, @"Data\BlankLinePaddingBeforeSingleLineComments_Cleaned.cs");
+        }
+
+        [TestMethod]
+        [HostType("VS IDE")]
+        public void CleaningInsertBlankLinePaddingBeforeSingleLineComments_DoesNothingOnSecondPass()
+        {
+            Settings.Default.Cleaning_InsertBlankLinePaddingBeforeSingleLineComments = true;
+
+            CleaningTestHelper.ExecuteCommandTwiceAndVerifyNoChangesOnSecondPass(RunInsertBlankLinePaddingBeforeSingleLineComments, _projectItem);
+        }
+
+        [TestMethod]
+        [HostType("VS IDE")]
+        public void CleaningInsertBlankLinePaddingBeforeSingleLineComments_DoesNothingWhenSettingIsDisabled()
+        {
+            Settings.Default.Cleaning_InsertBlankLinePaddingBeforeSingleLineComments = false;
+
+            CleaningTestHelper.ExecuteCommandAndVerifyNoChanges(RunInsertBlankLinePaddingBeforeSingleLineComments, _projectItem);
+        }
+
+        #endregion Tests
+
+        #region Helpers
+
+        private static void RunInsertBlankLinePaddingBeforeSingleLineComments(Document document)
+        {
+            var textDocument = TestUtils.GetTextDocument(document);
+
+            _insertBlankLinePaddingLogic.InsertPaddingBeforeSingleLineComments(textDocument);
+        }
+
+        #endregion Helpers
+    }
+}
---
+++ CodeMaid.IntegrationTests/Cleaning/Insert/Data/BlankLinePaddingBeforeSingleLineComments_Cleaned.cs
@@ -0,0 +1,28 @@
+﻿namespace SteveCadwallader.CodeMaid.IntegrationTests.Cleaning.Insert.Data
+{
+    public class BlankLinePaddingBeforeSingleLineComments
+    {
+        // Single line comment after brace does not insert padding.
+        // Additional comments next to another single line comment does not insert padding.
+
+        private bool _field;
+
+        // Floating single line comment inserts padding.
+        // But not on the following line as well.
+
+        private bool _field2;
+        //// Quadruple slashed comments are ignored (i.e. commented out comments).
+
+        private void Method()
+        {
+            if (true)
+            {
+            }
+
+            // Comments hanging between sections should be padded.
+            if (true)
+            {
+            }
+        }
+    }
+}
