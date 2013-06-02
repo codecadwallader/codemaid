24c7ac0d7bc6176090e4562612faea9d3cd2dd05
s_cadwallader <steve.cadwallader@gmail.com>
2013-06-02 08:09:15 -0400

    Extend insert explicit access modifiers on properties unit tests to verify properties inside an interface and explicit interface implementations are ignored.

--- CodeMaid.IntegrationTests/Cleaning/Insert/Data/ExplicitAccessModifiersOnProperties_Cleaned.cs
+++ CodeMaid.IntegrationTests/Cleaning/Insert/Data/ExplicitAccessModifiersOnProperties_Cleaned.cs
@@ -1,6 +1,6 @@
 ﻿namespace SteveCadwallader.CodeMaid.IntegrationTests.Cleaning.Insert.Data
 {
-    public class ExplicitAccessModifiersOnProperties
+    public class ExplicitAccessModifiersOnProperties : IExplicitAccessModifiersOnProperties
     {
         private bool UnspecifiedProperty { get; set; }
 
@@ -11,5 +11,21 @@
         protected bool ProtectedProperty { get; set; }
 
         private bool PrivateProperty { get; set; }
+
+        public bool PrivateGetterProperty { private get; set; }
+
+        public bool PrivateSetterProperty { get; private set; }
+
+        // Explicit interface implementations should not be given an explicit access modifier.
+        bool IExplicitAccessModifiersOnProperties.PropertyInInterface
+        {
+            get { return false; }
+        }
+    }
+
+    public interface IExplicitAccessModifiersOnProperties
+    {
+        // Properties in an interface should not be given an explicit access modifier.
+        bool PropertyInInterface { get; }
     }
 }
