a36424e97e3a63c1584b48c4d165f80bc7428ce7
s_cadwallader <steve.cadwallader@gmail.com>
2013-06-02 08:00:40 -0400

    Extend insert explicit access modifiers on methods unit tests to verify static constructors, destructors, explicit interface implementations and method definitions in an interface are all ignored.

--- CodeMaid.IntegrationTests/Cleaning/Insert/Data/ExplicitAccessModifiersOnMethods_Cleaned.cs
+++ CodeMaid.IntegrationTests/Cleaning/Insert/Data/ExplicitAccessModifiersOnMethods_Cleaned.cs
@@ -1,7 +1,17 @@
 ﻿namespace SteveCadwallader.CodeMaid.IntegrationTests.Cleaning.Insert.Data
 {
-    public class ExplicitAccessModifiersOnMethods
+    public class ExplicitAccessModifiersOnMethods : IExplicitAccessModifiersOnMethods
     {
+        // Skip static constructors - they should not have an access modifier.
+        static ExplicitAccessModifiersOnMethods()
+        {
+        }
+
+        // Skip destructors - they should not have an access modifier.
+        ~ExplicitAccessModifiersOnMethods()
+        {
+        }
+
         private void UnspecifiedMethod()
         {
         }
@@ -21,5 +31,21 @@
         private void PrivateMethod()
         {
         }
+
+        // Explicit interface implementations should not be given an explicit access modifier.
+        void IExplicitAccessModifiersOnMethods.MethodInInterface()
+        {
+        }
+
+        // Skip partial methods - access modifier may be specified elsewhere.
+        partial void PartialMethod()
+        {
+        }
+    }
+
+    public interface IExplicitAccessModifiersOnMethods
+    {
+        // Methods in an interface should not be given an explicit access modifier.
+        void MethodInInterface();
     }
 }
