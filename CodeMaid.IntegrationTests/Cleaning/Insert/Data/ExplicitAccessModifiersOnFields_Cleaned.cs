57c5feda1ae006628af4141880f86b47afbd992e
s_cadwallader <steve.cadwallader@gmail.com>
2013-06-02 07:19:41 -0400

    Add unit tests for insert explicit access modifiers on fields.

---
+++ CodeMaid.IntegrationTests/Cleaning/Insert/Data/ExplicitAccessModifiersOnFields_Cleaned.cs
@@ -0,0 +1,11 @@
+﻿namespace SteveCadwallader.CodeMaid.IntegrationTests.Cleaning.Insert.Data
+{
+    public class ExplicitAccessModifiersOnFields
+    {
+        private bool UnspecifiedField;
+        public bool PublicField;
+        internal bool InternalField;
+        protected bool ProtectedField;
+        private bool PrivateField;
+    }
+}
---
+++ CodeMaid.IntegrationTests/Cleaning/Insert/ExplicitAccessModifiersOnFieldsTests.cs
@@ -0,0 +1,100 @@
+﻿#region CodeMaid is Copyright 2007-2013 Steve Cadwallader.
+
+// CodeMaid is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License version 3
+// as published by the Free Software Foundation.
+//
+// CodeMaid is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details <http://www.gnu.org/licenses/>.
+
+#endregion CodeMaid is Copyright 2007-2013 Steve Cadwallader.
+
+using System.Linq;
+using EnvDTE;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SteveCadwallader.CodeMaid.Helpers;
+using SteveCadwallader.CodeMaid.IntegrationTests.Helpers;
+using SteveCadwallader.CodeMaid.Logic.Cleaning;
+using SteveCadwallader.CodeMaid.Model.CodeItems;
+using SteveCadwallader.CodeMaid.Properties;
+
+namespace SteveCadwallader.CodeMaid.IntegrationTests.Cleaning.Insert
+{
+    [TestClass]
+    [DeploymentItem(@"Cleaning\Insert\Data\ExplicitAccessModifiersOnFields.cs", "Data")]
+    [DeploymentItem(@"Cleaning\Insert\Data\ExplicitAccessModifiersOnFields_Cleaned.cs", "Data")]
+    public class ExplicitAccessModifiersOnFieldsTests
+    {
+        #region Setup
+
+        private static InsertExplicitAccessModifierLogic _insertExplicitAccessModifierLogic;
+        private ProjectItem _projectItem;
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext testContext)
+        {
+            _insertExplicitAccessModifierLogic = InsertExplicitAccessModifierLogic.GetInstance();
+            Assert.IsNotNull(_insertExplicitAccessModifierLogic);
+        }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            TestEnvironment.CommonTestInitialize();
+            _projectItem = TestEnvironment.LoadFileIntoProject(@"Data\ExplicitAccessModifiersOnFields.cs");
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            TestEnvironment.RemoveFromProject(_projectItem);
+        }
+
+        #endregion Setup
+
+        #region Tests
+
+        [TestMethod]
+        [HostType("VS IDE")]
+        public void CleaningInsertExplicitAccessModifiersOnFields_CleansAsExpected()
+        {
+            Settings.Default.Cleaning_InsertExplicitAccessModifiersOnFields = true;
+
+            CleaningTestHelper.ExecuteCommandAndVerifyResults(RunInsertExplicitAccessModifiersOnFields, _projectItem, @"Data\ExplicitAccessModifiersOnFields_Cleaned.cs");
+        }
+
+        [TestMethod]
+        [HostType("VS IDE")]
+        public void CleaningInsertExplicitAccessModifiersOnFields_DoesNothingOnSecondPass()
+        {
+            Settings.Default.Cleaning_InsertExplicitAccessModifiersOnFields = true;
+
+            CleaningTestHelper.ExecuteCommandTwiceAndVerifyNoChangesOnSecondPass(RunInsertExplicitAccessModifiersOnFields, _projectItem);
+        }
+
+        [TestMethod]
+        [HostType("VS IDE")]
+        public void CleaningInsertExplicitAccessModifiersOnFields_DoesNothingWhenSettingIsDisabled()
+        {
+            Settings.Default.Cleaning_InsertExplicitAccessModifiersOnFields = false;
+
+            CleaningTestHelper.ExecuteCommandAndVerifyNoChanges(RunInsertExplicitAccessModifiersOnFields, _projectItem);
+        }
+
+        #endregion Tests
+
+        #region Helpers
+
+        private static void RunInsertExplicitAccessModifiersOnFields(Document document)
+        {
+            var codeItems = CodeModelHelper.RetrieveCodeItemsIncludingRegions(document);
+            var fields = codeItems.OfType<CodeItemField>().ToList();
+
+            _insertExplicitAccessModifierLogic.InsertExplicitAccessModifiersOnFields(fields);
+        }
+
+        #endregion Helpers
+    }
+}
