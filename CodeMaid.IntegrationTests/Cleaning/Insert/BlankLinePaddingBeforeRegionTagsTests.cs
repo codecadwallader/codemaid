6a54ad44cd5ce380382729b34f41a7cd3430b6c4
s_cadwallader <steve.cadwallader@gmail.com>
2013-06-02 08:33:03 -0400

    Add unit tests for insert blank line padding before region tags.

---
+++ CodeMaid.IntegrationTests/Cleaning/Insert/BlankLinePaddingBeforeRegionTagsTests.cs
@@ -0,0 +1,100 @@
+﻿#region CodeMaid is Copyright 2007-2013 Steve Cadwallader.
+
+// CodeMaid is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License version 3
+// as published by the Free Software Foundation.
+//
+// CodeMaid is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details <http://www.gnu.org/licenses/>.
+
+#endregion CodeMaid is Copyright 2007-2013 Steve Cadwallader.
+
+using System.Linq;
+using EnvDTE;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SteveCadwallader.CodeMaid.Helpers;
+using SteveCadwallader.CodeMaid.IntegrationTests.Helpers;
+using SteveCadwallader.CodeMaid.Logic.Cleaning;
+using SteveCadwallader.CodeMaid.Model.CodeItems;
+using SteveCadwallader.CodeMaid.Properties;
+
+namespace SteveCadwallader.CodeMaid.IntegrationTests.Cleaning.Insert
+{
+    [TestClass]
+    [DeploymentItem(@"Cleaning\Insert\Data\BlankLinePaddingBeforeRegionTags.cs", "Data")]
+    [DeploymentItem(@"Cleaning\Insert\Data\BlankLinePaddingBeforeRegionTags_Cleaned.cs", "Data")]
+    public class BlankLinePaddingBeforeRegionTagsTests
+    {
+        #region Setup
+
+        private static InsertBlankLinePaddingLogic _insertBlankLinePaddingLogic;
+        private ProjectItem _projectItem;
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext testContext)
+        {
+            _insertBlankLinePaddingLogic = InsertBlankLinePaddingLogic.GetInstance(TestEnvironment.Package);
+            Assert.IsNotNull(_insertBlankLinePaddingLogic);
+        }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            TestEnvironment.CommonTestInitialize();
+            _projectItem = TestEnvironment.LoadFileIntoProject(@"Data\BlankLinePaddingBeforeRegionTags.cs");
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            TestEnvironment.RemoveFromProject(_projectItem);
+        }
+
+        #endregion Setup
+
+        #region Tests
+
+        [TestMethod]
+        [HostType("VS IDE")]
+        public void CleaningInsertBlankLinePaddingBeforeRegionTags_CleansAsExpected()
+        {
+            Settings.Default.Cleaning_InsertBlankLinePaddingBeforeRegionTags = true;
+
+            CleaningTestHelper.ExecuteCommandAndVerifyResults(RunInsertBlankLinePaddingBeforeRegionTags, _projectItem, @"Data\BlankLinePaddingBeforeRegionTags_Cleaned.cs");
+        }
+
+        [TestMethod]
+        [HostType("VS IDE")]
+        public void CleaningInsertBlankLinePaddingBeforeRegionTags_DoesNothingOnSecondPass()
+        {
+            Settings.Default.Cleaning_InsertBlankLinePaddingBeforeRegionTags = true;
+
+            CleaningTestHelper.ExecuteCommandTwiceAndVerifyNoChangesOnSecondPass(RunInsertBlankLinePaddingBeforeRegionTags, _projectItem);
+        }
+
+        [TestMethod]
+        [HostType("VS IDE")]
+        public void CleaningInsertBlankLinePaddingBeforeRegionTags_DoesNothingWhenSettingIsDisabled()
+        {
+            Settings.Default.Cleaning_InsertBlankLinePaddingBeforeRegionTags = false;
+
+            CleaningTestHelper.ExecuteCommandAndVerifyNoChanges(RunInsertBlankLinePaddingBeforeRegionTags, _projectItem);
+        }
+
+        #endregion Tests
+
+        #region Helpers
+
+        private static void RunInsertBlankLinePaddingBeforeRegionTags(Document document)
+        {
+            var codeItems = CodeModelHelper.RetrieveCodeItemsIncludingRegions(document);
+            var regions = codeItems.OfType<CodeItemRegion>().ToList();
+
+            _insertBlankLinePaddingLogic.InsertPaddingBeforeRegionTags(regions);
+        }
+
+        #endregion Helpers
+    }
+}
---
+++ CodeMaid.IntegrationTests/Cleaning/Insert/Data/BlankLinePaddingBeforeRegionTags_Cleaned.cs
@@ -0,0 +1,17 @@
+﻿namespace SteveCadwallader.CodeMaid.IntegrationTests.Cleaning.Insert.Data
+{
+    public class BlankLinePaddingBeforeRegionTags
+    {
+        #region Region One
+        #endregion
+
+        #region Region Two
+
+        #region Nested Region
+        #endregion
+        #endregion
+
+        #region Region Three
+        #endregion
+    }
+}
