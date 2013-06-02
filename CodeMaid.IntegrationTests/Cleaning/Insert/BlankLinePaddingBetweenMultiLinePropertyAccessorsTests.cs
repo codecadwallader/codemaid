75e97ee6594178bde3bc51f7ab700c84b388df5b
s_cadwallader <steve.cadwallader@gmail.com>
2013-06-02 11:31:58 -0400

    Add unit tests for inserting blank line padding between multi-line property accessors.

---
+++ CodeMaid.IntegrationTests/Cleaning/Insert/BlankLinePaddingBetweenMultiLinePropertyAccessorsTests.cs
@@ -0,0 +1,100 @@
+﻿#region CodeMaid is Copyright 2007-2013 Steve Cadwallader.
+
+// CodeMaid is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License version 3
+// as published by the Free Software Foundation.
+//
+// CodeMaid is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details <http://www.gnu.org/licenses/>.
+
+#endregion CodeMaid is Copyright 2007-2013 Steve Cadwallader.
+
+using System.Linq;
+using EnvDTE;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SteveCadwallader.CodeMaid.Helpers;
+using SteveCadwallader.CodeMaid.IntegrationTests.Helpers;
+using SteveCadwallader.CodeMaid.Logic.Cleaning;
+using SteveCadwallader.CodeMaid.Model.CodeItems;
+using SteveCadwallader.CodeMaid.Properties;
+
+namespace SteveCadwallader.CodeMaid.IntegrationTests.Cleaning.Insert
+{
+    [TestClass]
+    [DeploymentItem(@"Cleaning\Insert\Data\BlankLinePaddingBetweenMultiLinePropertyAccessors.cs", "Data")]
+    [DeploymentItem(@"Cleaning\Insert\Data\BlankLinePaddingBetweenMultiLinePropertyAccessors_Cleaned.cs", "Data")]
+    public class BlankLinePaddingBetweenMultiLinePropertyAccessorsTests
+    {
+        #region Setup
+
+        private static InsertBlankLinePaddingLogic _insertBlankLinePaddingLogic;
+        private ProjectItem _projectItem;
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext testContext)
+        {
+            _insertBlankLinePaddingLogic = InsertBlankLinePaddingLogic.GetInstance(TestEnvironment.Package);
+            Assert.IsNotNull(_insertBlankLinePaddingLogic);
+        }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            TestEnvironment.CommonTestInitialize();
+            _projectItem = TestEnvironment.LoadFileIntoProject(@"Data\BlankLinePaddingBetweenMultiLinePropertyAccessors.cs");
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            TestEnvironment.RemoveFromProject(_projectItem);
+        }
+
+        #endregion Setup
+
+        #region Tests
+
+        [TestMethod]
+        [HostType("VS IDE")]
+        public void CleaningInsertBlankLinePaddingBetweenMultiLinePropertyAccessors_CleansAsExpected()
+        {
+            Settings.Default.Cleaning_InsertBlankLinePaddingBetweenPropertiesMultiLineAccessors = true;
+
+            CleaningTestHelper.ExecuteCommandAndVerifyResults(RunInsertBlankLinePaddingBetweenMultiLinePropertyAccessors, _projectItem, @"Data\BlankLinePaddingBetweenMultiLinePropertyAccessors_Cleaned.cs");
+        }
+
+        [TestMethod]
+        [HostType("VS IDE")]
+        public void CleaningInsertBlankLinePaddingBetweenMultiLinePropertyAccessors_DoesNothingOnSecondPass()
+        {
+            Settings.Default.Cleaning_InsertBlankLinePaddingBetweenPropertiesMultiLineAccessors = true;
+
+            CleaningTestHelper.ExecuteCommandTwiceAndVerifyNoChangesOnSecondPass(RunInsertBlankLinePaddingBetweenMultiLinePropertyAccessors, _projectItem);
+        }
+
+        [TestMethod]
+        [HostType("VS IDE")]
+        public void CleaningInsertBlankLinePaddingBetweenMultiLinePropertyAccessors_DoesNothingWhenSettingIsDisabled()
+        {
+            Settings.Default.Cleaning_InsertBlankLinePaddingBetweenPropertiesMultiLineAccessors = false;
+
+            CleaningTestHelper.ExecuteCommandAndVerifyNoChanges(RunInsertBlankLinePaddingBetweenMultiLinePropertyAccessors, _projectItem);
+        }
+
+        #endregion Tests
+
+        #region Helpers
+
+        private static void RunInsertBlankLinePaddingBetweenMultiLinePropertyAccessors(Document document)
+        {
+            var codeItems = CodeModelHelper.RetrieveCodeItemsIncludingRegions(document);
+            var properties = codeItems.OfType<CodeItemProperty>().ToList();
+
+            _insertBlankLinePaddingLogic.InsertPaddingBetweenMultiLinePropertyAccessors(properties);
+        }
+
+        #endregion Helpers
+    }
+}
---
+++ CodeMaid.IntegrationTests/Cleaning/Insert/Data/BlankLinePaddingBetweenMultiLinePropertyAccessors_Cleaned.cs
@@ -0,0 +1,127 @@
+﻿namespace SteveCadwallader.CodeMaid.IntegrationTests.Cleaning.Insert.Data
+{
+    public class BlankLinePaddingBetweenMultiLinePropertyAccessors
+    {
+        private bool _field;
+        
+        public bool AutoImplementedProperty { get; set; }
+
+        public bool SingleLineProperty
+        {
+            get { return _field; }
+            set { _field = value; }
+        }
+
+        public bool SingleLineGetterMultiLineBodySetter
+        {
+            get { return _field; }
+
+            set
+            {
+                if (_field != value)
+                {
+                    _field = value;
+                }
+            }
+        }
+
+        public bool MultiLineGetterSingleLineSetter
+        {
+            get
+            {
+                if (_field == false)
+                {
+                    _field = true;
+                }
+
+                return _field;
+            }
+
+            set { _field = value; }
+        }
+
+        public bool MultiLineGetterMultiLineSetter
+        {
+            get
+            {
+                if (_field == false)
+                {
+                    _field = true;
+                }
+
+                return _field;
+            }
+
+            set
+            {
+                if (_field != value)
+                {
+                    _field = value;
+                }
+            }
+        }
+
+        public bool MultiLineSetterMultiLineGetter
+        {
+            set
+            {
+                if (_field != value)
+                {
+                    _field = value;
+                }
+            }
+
+            get
+            {
+                if (_field == false)
+                {
+                    _field = true;
+                }
+
+                return _field;
+            }
+        }
+
+        public bool MultiLineGetterMultiLineSetter2
+        {
+            get
+            {
+                if (_field == false)
+                {
+                    _field = true;
+                }
+
+                return _field;
+            }
+
+            set
+            {
+                if (_field != value)
+                {
+                    _field = value;
+                }
+            }
+        }
+
+        public bool MultiLineSetterMultiLineGetter2
+        {
+            set
+            {
+                if (_field != value)
+                {
+                    _field = value;
+                }
+            }
+
+            get
+            {
+                if (_field == false)
+                {
+                    _field = true;
+                }
+
+                return _field;
+            }
+        }
+    }
+}
