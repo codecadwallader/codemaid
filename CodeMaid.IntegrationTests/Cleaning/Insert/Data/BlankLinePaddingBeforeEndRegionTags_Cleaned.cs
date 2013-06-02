ecd6d6635fde23458ef8cb6649463c24626d0351
s_cadwallader <steve.cadwallader@gmail.com>
2013-06-02 08:39:01 -0400

    Add unit tests for insert blank line padding before endregion tags.

---
+++ CodeMaid.IntegrationTests/Cleaning/Insert/Data/BlankLinePaddingBeforeEndRegionTags_Cleaned.cs
@@ -0,0 +1,18 @@
+﻿namespace SteveCadwallader.CodeMaid.IntegrationTests.Cleaning.Insert.Data
+{
+    public class BlankLinePaddingBeforeEndRegionTags
+    {
+        #region Region One
+
+        #endregion
+        #region Region Two
+        #region Nested Region
+
+        #endregion
+
+        #endregion
+        #region Region Three
+
+        #endregion
+    }
+}
