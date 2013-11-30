8f9eb8f6f09bccd2f8a1551686fad0ecc5982aaa
Steve Cadwallader <steve.cadwallader@gmail.com>
2013-11-30 14:55:22 -0500

    Add unit tests for comment formatting, both standard and XML.

---
+++ CodeMaid.IntegrationTests/Cleaning/Comments/Data/StandardCommentFormat.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.Serialization;
+
+namespace SteveCadwallader.CodeMaid.IntegrationTests.Cleaning.Comments.Data
+{
+    [DataContract(Name = "SecurityContext", Namespace = "http://schemas.datacontract.org/2004/07/Example")]
+    public class StandardCommentFormat
+    {
+        // This is a traditional comment that started on column nine and will go on to column one hundred and twenty three.
+
+        //This comment has no space after the comment prefix, so it should be treated as code and ignored instead of formatted.
+
+		// This comment is led with tabs instead of spaces and it is expected that this line and those following it contain the same indentation style.
+
+        public void Method()
+        {
+            string s = "This is a string that includes the comment prefix // but it should not trigger any re-formatting.";
+        }
+    }
+}
---
+++ CodeMaid.IntegrationTests/Cleaning/Comments/StandardCommentFormatTests.cs
@@ -0,0 +1,96 @@
+﻿#region CodeMaid is Copyright 2007-2013 Steve Cadwallader.
+
+// CodeMaid is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License version 3
+// as published by the Free Software Foundation.
+//
+// CodeMaid is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details <http://www.gnu.org/licenses/>.
+
+#endregion CodeMaid is Copyright 2007-2013 Steve Cadwallader.
+
+using EnvDTE;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SteveCadwallader.CodeMaid.IntegrationTests.Helpers;
+using SteveCadwallader.CodeMaid.Logic.Cleaning;
+using SteveCadwallader.CodeMaid.Properties;
+
+namespace SteveCadwallader.CodeMaid.IntegrationTests.Cleaning.Comments
+{
+    [TestClass]
+    [DeploymentItem(@"Cleaning\Comments\Data\StandardCommentFormat.cs", "Data")]
+    [DeploymentItem(@"Cleaning\Comments\Data\StandardCommentFormat_Cleaned.cs", "Data")]
+    public class StandardCommentFormatTests
+    {
+        #region Setup
+
+        private static CommentFormatLogic _commentFormatLogic;
+        private ProjectItem _projectItem;
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext testContext)
+        {
+            _commentFormatLogic = CommentFormatLogic.GetInstance(TestEnvironment.Package);
+            Assert.IsNotNull(_commentFormatLogic);
+        }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            TestEnvironment.CommonTestInitialize();
+            _projectItem = TestEnvironment.LoadFileIntoProject(@"Data\StandardCommentFormat.cs");
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            TestEnvironment.RemoveFromProject(_projectItem);
+        }
+
+        #endregion Setup
+
+        #region Tests
+
+        [TestMethod]
+        [HostType("VS IDE")]
+        public void CleaningFormatStandardComments_CleansAsExpected()
+        {
+            Settings.Default.Cleaning_CommentRunDuringCleanup = true;
+
+            CleaningTestHelper.ExecuteCommandAndVerifyResults(RunFormatComments, _projectItem, @"Data\StandardCommentFormat_Cleaned.cs");
+        }
+
+        [TestMethod]
+        [HostType("VS IDE")]
+        public void CleaningFormatStandardComments_DoesNothingOnSecondPass()
+        {
+            Settings.Default.Cleaning_CommentRunDuringCleanup = true;
+
+            CleaningTestHelper.ExecuteCommandTwiceAndVerifyNoChangesOnSecondPass(RunFormatComments, _projectItem);
+        }
+
+        [TestMethod]
+        [HostType("VS IDE")]
+        public void CleaningFormatStandardComments_DoesNothingWhenSettingIsDisabled()
+        {
+            Settings.Default.Cleaning_CommentRunDuringCleanup = false;
+
+            CleaningTestHelper.ExecuteCommandAndVerifyNoChanges(RunFormatComments, _projectItem);
+        }
+
+        #endregion Tests
+
+        #region Helpers
+
+        private static void RunFormatComments(Document document)
+        {
+            var textDocument = TestUtils.GetTextDocument(document);
+
+            _commentFormatLogic.FormatComments(textDocument);
+        }
+
+        #endregion Helpers
+    }
+}
