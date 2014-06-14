190849e985b27fb9d987d678b92f4b365444db71
Steve Cadwallader <steve.cadwallader@gmail.com>
2014-06-14 09:38:18 -0400

    Card 87: Add more unit tests around combined region removal and insertion logic, both with and without access modifiers specified.

---
+++ CodeMaid.IntegrationTests/Reorganizing/Data/RegionsRemoveAndInsertWithoutAccessModifiers_Reorganized.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace SteveCadwallader.CodeMaid.IntegrationTests.Reorganizing.Data
+{
+    public class RegionsRemoveAndInsertWithoutAccessModifiers
+    {
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ActiveDocumentRestorer" /> class.
+        /// </summary>
+        /// <param name="package">The hosting package.</param>
+        internal ActiveDocumentRestorer(CodeMaidPackage package)
+        {
+            Package = package;
+
+            StartTracking();
+        }
+
+        #endregion Constructors
+
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets the hosting package.
+        /// </summary>
+        private CodeMaidPackage Package { get; set; }
+
+        /// <summary>
+        /// Gets or sets the active document.
+        /// </summary>
+        private Document TrackedDocument { get; set; }
+
+        #endregion Properties
+
+        #region Methods
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting
+        /// unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+            RestoreTrackedDocument();
+        }
+
+        /// <summary>
+        /// Restores the tracked document if not already active.
+        /// </summary>
+        internal void RestoreTrackedDocument()
+        {
+            if (TrackedDocument != null && Package.ActiveDocument != TrackedDocument)
+            {
+                TrackedDocument.Activate();
+            }
+        }
+
+        /// <summary>
+        /// Starts tracking the active document.
+        /// </summary>
+        internal void StartTracking()
+        {
+            // Cache the active document.
+            TrackedDocument = Package.ActiveDocument;
+        }
+
+        #endregion Methods
+    }
+}
---
+++ CodeMaid.IntegrationTests/Reorganizing/RegionsRemoveAndInsertWithoutAccessModifiersTests.cs
@@ -0,0 +1,87 @@
+﻿#region CodeMaid is Copyright 2007-2014 Steve Cadwallader.
+
+// CodeMaid is free software: you can redistribute it and/or modify it under the terms of the GNU
+// Lesser General Public License version 3 as published by the Free Software Foundation.
+//
+// CodeMaid is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+// even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details <http://www.gnu.org/licenses/>.
+
+#endregion CodeMaid is Copyright 2007-2014 Steve Cadwallader.
+
+using EnvDTE;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SteveCadwallader.CodeMaid.IntegrationTests.Helpers;
+using SteveCadwallader.CodeMaid.Logic.Reorganizing;
+using SteveCadwallader.CodeMaid.Properties;
+
+namespace SteveCadwallader.CodeMaid.IntegrationTests.Reorganizing
+{
+    [TestClass]
+    [DeploymentItem(@"Reorganizing\Data\RegionsRemoveAndInsertWithoutAccessModifiers.cs", "Data")]
+    [DeploymentItem(@"Reorganizing\Data\RegionsRemoveAndInsertWithoutAccessModifiers_Reorganized.cs", "Data")]
+    public class RegionsRemoveAndInsertWithoutAccessModifiersTests
+    {
+        #region Setup
+
+        private static CodeReorderManager _codeReorderManager;
+        private ProjectItem _projectItem;
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext testContext)
+        {
+            _codeReorderManager = CodeReorderManager.GetInstance(TestEnvironment.Package);
+            Assert.IsNotNull(_codeReorderManager);
+        }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            TestEnvironment.CommonTestInitialize();
+            _projectItem = TestEnvironment.LoadFileIntoProject(@"Data\RegionsRemoveAndInsertWithoutAccessModifiers.cs");
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            TestEnvironment.RemoveFromProject(_projectItem);
+        }
+
+        #endregion Setup
+
+        #region Tests
+
+        [TestMethod]
+        [HostType("VS IDE")]
+        public void ReorganizingRegionsRemoveAndInsertWithoutAccessModifiers_ReorganizesAsExpected()
+        {
+            Settings.Default.Reorganizing_RegionsRemoveExistingRegions = true;
+            Settings.Default.Reorganizing_RegionsAutoGenerate = true;
+            Settings.Default.Reorganizing_RegionsIncludeAccessLevel = false;
+
+            TestOperations.ExecuteCommandAndVerifyResults(RunReorganize, _projectItem, @"Data\RegionsRemoveAndInsertWithoutAccessModifiers_Reorganized.cs");
+        }
+
+        [TestMethod]
+        [HostType("VS IDE")]
+        public void ReorganizingRegionsRemoveAndInsertWithoutAccessModifiers_DoesNothingOnSecondPass()
+        {
+            Settings.Default.Reorganizing_RegionsRemoveExistingRegions = true;
+            Settings.Default.Reorganizing_RegionsAutoGenerate = true;
+            Settings.Default.Reorganizing_RegionsIncludeAccessLevel = false;
+
+            TestOperations.ExecuteCommandTwiceAndVerifyNoChangesOnSecondPass(RunReorganize, _projectItem);
+        }
+
+        #endregion Tests
+
+        #region Helpers
+
+        private static void RunReorganize(Document document)
+        {
+            _codeReorderManager.Reorganize(document, false);
+        }
+
+        #endregion Helpers
+    }
+}
