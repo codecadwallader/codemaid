a1ef54619f9fcabb06ab9909f97e432fd38121e2
Steve Cadwallader <steve.cadwallader@gmail.com>
2014-06-14 08:16:50 -0400

    Card 87: Add unit tests for region insertion following a two field reorder swap.

---
+++ CodeMaid.IntegrationTests/Reorganizing/Data/RegionsInsertAfterReorder.cs
@@ -0,0 +1,9 @@
+﻿namespace SteveCadwallader.CodeMaid.IntegrationTests.Reorganizing.Data
+{
+    public class RegionsInsertAfterReorder
+    {
+        public const int ToolbarIDCodeMaidToolbarSpade = 0x1040;
+
+        public const int MenuIDCodeMaidContextSpade = 0x1060;
+    }
+}
---
+++ CodeMaid.IntegrationTests/Reorganizing/Data/RegionsInsertAfterReorder_Reorganized.cs
@@ -0,0 +1,12 @@
+﻿namespace SteveCadwallader.CodeMaid.IntegrationTests.Reorganizing.Data
+{
+    public class RegionsInsertAfterReorder
+    {
+        #region Fields
+
+        public const int MenuIDCodeMaidContextSpade = 0x1060;
+        public const int ToolbarIDCodeMaidToolbarSpade = 0x1040;
+
+        #endregion Fields
+    }
+}
