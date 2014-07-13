86e8ac20724abe4ae0454a935aab1c216212c58d
Steve Cadwallader <steve.cadwallader@gmail.com>
2014-07-13 11:42:40 -0400

    Card 142: Group all document context menu options into a CodeMaid submenu.

--- CodeMaid/Integration/GuidList.cs
+++ CodeMaid/Integration/GuidList.cs
@@ -50,19 +50,6 @@ namespace SteveCadwallader.CodeMaid.Integration
         public static readonly Guid GuidCodeMaidContextSpadeFindGroup = new Guid("4e3fd2e6-4cde-497c-a899-eee7e53c8852");
         public static readonly Guid GuidCodeMaidContextSpadeDeleteGroup = new Guid("e4abf3d9-e332-4c5d-97a9-4ac09395c28f");
 
-        // Miscellaneous context menu group guids.
-
-        public static readonly Guid GuidCodeMaidContextASPXWindowGroup = new Guid("d4cee201-6b18-4265-8693-a0cb38cf070b");
-        public static readonly Guid GuidCodeMaidContextASPXCodeWindowGroup = new Guid("9c3012ad-61b1-4272-9ea2-5c07e33a9e2f");
-        public static readonly Guid GuidCodeMaidContextCodeWindowGroup = new Guid("29039753-26f9-4548-b81c-d7baa5ebc661");
-        public static readonly Guid GuidCodeMaidContextCSSWindowGroup = new Guid("5c88b2b2-24c7-4ff5-b16c-19f01e46a229");
-        public static readonly Guid GuidCodeMaidContextCSSDocumentGroup = new Guid("8b375766-4cfd-4a2d-9e15-a535304efe2b");
-        public static readonly Guid GuidCodeMaidContextDocumentTabGroup = new Guid("9cba7ef0-ce80-42bb-9f25-c69a9710d328");
-        public static readonly Guid GuidCodeMaidContextHTMLWindowGroup = new Guid("248aaae5-8d3e-4914-9cd9-89c95941965a");
-        public static readonly Guid GuidCodeMaidContextHTMLDocumentGroup = new Guid("5d667016-1652-4c9e-b57e-66c94ba59f19");
-        public static readonly Guid GuidCodeMaidContextScriptWindowGroup = new Guid("b6343c4a-01bd-4a5e-b1fb-9a8a6203c74c");
-        public static readonly Guid GuidCodeMaidContextXAMLWindowGroup = new Guid("b362d22f-6a03-4247-aaf9-bfc02f81d41b");
-
         // Solution explorer context menu group guids.
 
         public static readonly Guid GuidCodeMaidContextSolutionNodeGroup = new Guid("eda37cf1-ff29-46aa-ba07-05abf7ba6596");
@@ -70,6 +57,10 @@ namespace SteveCadwallader.CodeMaid.Integration
         public static readonly Guid GuidCodeMaidContextProjectNodeGroup = new Guid("a04159ed-c5ee-4146-954a-4e662b297513");
         public static readonly Guid GuidCodeMaidContextFolderNodeGroup = new Guid("9979d6f0-4af6-446a-a9ff-b84e27452435");
         public static readonly Guid GuidCodeMaidContextItemNodeGroup = new Guid("36669853-51f0-4da1-828d-d43d67b8450c");
+
+        // Document tab context menu group guids.
+
+        public static readonly Guid GuidCodeMaidContextDocumentTabGroup = new Guid("9cba7ef0-ce80-42bb-9f25-c69a9710d328");
 
         // Command guids.
 
