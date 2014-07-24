1accf8b5b0eaffc602f1ac180f9e7b4f21c1403c
Willem Duncan <willem@quadspeed.com>
2014-07-24 11:39:37 +0200

    Added code formatting unit tests.

---
+++ CodeMaid.UnitTests/Formatting/CommentFormatHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SteveCadwallader.CodeMaid.Helpers;
+using SteveCadwallader.CodeMaid.Model.Comments;
+
+namespace SteveCadwallader.CodeMaid.UnitTests.Formatting
+{
+    internal class CommentFormatHelper
+    {
+        public static string Format(IEnumerable<string> text, CodeCommentOptions options)
+        {
+            return Format(string.Join(Environment.NewLine, text), options);
+        }
+
+        public static string Format(string text, CodeCommentOptions options)
+        {
+            var xml = XElement.Parse(string.Format("<doc>{0}</doc>", text));
+            var line = new CommentLineXml(xml, options);
+            var regex = CodeCommentHelper.GetCommentRegex("CSharp", false);
+            var formatter = new CommentFormatter(line, string.Empty, options, regex);
+            return formatter.ToString();
+        }
+
+
+        public static void AssertEqualAfterFormat(string input, CodeCommentOptions options)
+        {
+            AssertEqualAfterFormat(input, input, options);
+        }
+
+        public static void AssertEqualAfterFormat(string input, string expected, CodeCommentOptions options)
+        {
+            var result = Format(input, options);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ CodeMaid.UnitTests/Formatting/XmlFormattingTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SteveCadwallader.CodeMaid.Model.Comments;
+
+namespace SteveCadwallader.CodeMaid.UnitTests.Formatting
+{
+    /// <summary>
+    /// Class with simple unit tests for formatting XML based comments. This calls the formatter
+    /// directly, rather than invoking it through the UI as with the integration tests.
+    /// </summary>
+    [TestClass]
+    public class XmlFormattingTests
+    {
+        [TestMethod]
+        [TestCategory("Formatting UnitTests")]
+        public void XmlFormattingTests_AllRootLevelTagsOnNewLine()
+        {
+            var input = "<summary></summary><returns></returns>";
+            var expected = "<summary></summary>" + Environment.NewLine + "<returns></returns>";
+
+            CommentFormatHelper.AssertEqualAfterFormat(input, expected, new CodeCommentOptions()
+            {
+                XmlBreakSummaryTag = false,
+                XmlBreakAllTags = false
+            });
+        }
+
+        [TestMethod]
+        [TestCategory("Formatting UnitTests")]
+        public void XmlFormattingTests_BreakAllTags()
+        {
+            var input = "<summary></summary><returns></returns>";
+            var expected = "<summary>" + Environment.NewLine + "</summary>" + Environment.NewLine + "<returns>" + Environment.NewLine + "</returns>";
+
+            CommentFormatHelper.AssertEqualAfterFormat(input, expected, new CodeCommentOptions()
+            {
+                XmlBreakSummaryTag = false,
+                XmlBreakAllTags = true
+            });
+        }
+
+        [TestMethod]
+        [TestCategory("Formatting UnitTests")]
+        public void XmlFormattingTests_BreakSummaryTags()
+        {
+            var input = "<summary></summary><returns></returns>";
+            var expected = "<summary>" + Environment.NewLine + "</summary>" + Environment.NewLine + "<returns></returns>";
+
+            CommentFormatHelper.AssertEqualAfterFormat(input, expected, new CodeCommentOptions()
+            {
+                XmlBreakSummaryTag = true,
+                XmlBreakAllTags = false
+            });
+        }
+
+        [TestMethod]
+        [TestCategory("Formatting UnitTests")]
+        public void XmlFormattingTests_SeperateClosingTagOnRootLevel()
+        {
+            var input = "<summary/>";
+            var expected = "<summary></summary>";
+
+            CommentFormatHelper.AssertEqualAfterFormat(input, expected, new CodeCommentOptions()
+            {
+                XmlBreakSummaryTag = false,
+                XmlBreakAllTags = false
+            });
+        }
+    }
+}
