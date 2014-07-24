e2084d23d26d5c165bb44ed8195aef9ef0d771d0
Willem Duncan <willem@quadspeed.com>
2014-07-24 12:31:27 +0200

    Added copyright headers to the new files.

--- CodeMaid/Model/Comments/CommentLineXml.cs
+++ CodeMaid/Model/Comments/CommentLineXml.cs
@@ -1,4 +1,15 @@
-﻿using System;
+﻿#region CodeMaid is Copyright 2007-2014 Steve Cadwallader.
+
+// CodeMaid is free software: you can redistribute it and/or modify it under the terms of the GNU
+// Lesser General Public License version 3 as published by the Free Software Foundation.
+//
+// CodeMaid is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+// even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details <http://www.gnu.org/licenses/>.
+
+#endregion CodeMaid is Copyright 2007-2014 Steve Cadwallader.
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
