0deb8ea46f9a3b2a5afa17a293f0e59f90aa678b
Steve Cadwallader <steve.cadwallader@gmail.com>
2014-08-25 06:27:16 -0400

    Resolve #18 by extending C++ exception case around removal of EOF trailing new line to also skip on .rc2 files.

--- CodeMaid/Logic/Cleaning/RemoveWhitespaceLogic.cs
+++ CodeMaid/Logic/Cleaning/RemoveWhitespaceLogic.cs
@@ -215,7 +215,8 @@ namespace SteveCadwallader.CodeMaid.Logic.Cleaning
             if (cursor.AtEndOfDocument && cursor.AtStartOfLine && cursor.AtEndOfLine)
             {
                 // Make an exception for C++ resource files to work-around known EOF issue: http://connect.microsoft.com/VisualStudio/feedback/details/173903/resource-compiler-returns-a-rc1004-unexpected-eof-found-error#details
-                if (textDocument.Language == "C/C++" && textDocument.Parent.FullName.EndsWith(".h"))
+                if (textDocument.Language == "C/C++" && 
+                    (textDocument.Parent.FullName.EndsWith(".h") || textDocument.Parent.FullName.EndsWith(".rc2")))
                 {
                     return;
                 }
