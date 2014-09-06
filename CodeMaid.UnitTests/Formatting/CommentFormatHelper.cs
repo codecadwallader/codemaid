9f2a80114a80f02711ef4947eab74513db543212
Steve Cadwallader <steve.cadwallader@gmail.com>
2014-09-06 08:26:28 -0400

    Issue 21: Add HeaderFormatting unit tests to extend existing integration tests.  In particular scenarios around hyphen lines that are not currently handled.  Tests fail at this point.

--- CodeMaid.UnitTests/Formatting/CommentFormatHelper.cs
+++ CodeMaid.UnitTests/Formatting/CommentFormatHelper.cs
@@ -1,9 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Xml.Linq;
+﻿#region CodeMaid is Copyright 2007-2014 Steve Cadwallader.
+
+// CodeMaid is free software: you can redistribute it and/or modify it under the terms of the GNU
+// Lesser General Public License version 3 as published by the Free Software Foundation.
+// 
+// CodeMaid is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+// even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details <http://www.gnu.org/licenses/>.
+
+#endregion CodeMaid is Copyright 2007-2014 Steve Cadwallader.
+
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SteveCadwallader.CodeMaid.Helpers;
 using SteveCadwallader.CodeMaid.Model.Comments;
+using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
 
 namespace SteveCadwallader.CodeMaid.UnitTests.Formatting
 {
--- CodeMaid.UnitTests/Formatting/XmlFormattingTests.cs
+++ CodeMaid.UnitTests/Formatting/XmlFormattingTests.cs
@@ -1,7 +1,18 @@
-﻿using System;
+﻿#region CodeMaid is Copyright 2007-2014 Steve Cadwallader.
+
+// CodeMaid is free software: you can redistribute it and/or modify it under the terms of the GNU
+// Lesser General Public License version 3 as published by the Free Software Foundation.
+//
+// CodeMaid is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+// even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details <http://www.gnu.org/licenses/>.
+
+#endregion CodeMaid is Copyright 2007-2014 Steve Cadwallader.
+
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SteveCadwallader.CodeMaid.Model.Comments;
 using SteveCadwallader.CodeMaid.Properties;
+using System;
 
 namespace SteveCadwallader.CodeMaid.UnitTests.Formatting
 {
