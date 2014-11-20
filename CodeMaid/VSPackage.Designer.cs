908f894b28d147a5bf0577796f82108b1d825864
Steve Cadwallader <steve.cadwallader@gmail.com>
2014-11-19 21:26:24 -0500

    Update to v0.7.4.3-beta label.

--- CodeMaid/VSPackage.Designer.cs
+++ CodeMaid/VSPackage.Designer.cs
@@ -81,7 +81,7 @@ namespace SteveCadwallader.CodeMaid {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to 0.7.4.2.
+        ///   Looks up a localized string similar to 0.7.4.3.
         /// </summary>
         internal static string _114 {
             get {
