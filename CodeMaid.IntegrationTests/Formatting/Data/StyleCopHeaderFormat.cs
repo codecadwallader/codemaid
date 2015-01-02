8970d3db09f085996e46ef72231da0e255060168
Steve Cadwallader <steve.cadwallader@gmail.com>
2015-01-02 11:21:42 -0500

    Replace 2014 copyright statements with 2015.

--- CodeMaid.IntegrationTests/Formatting/Data/StyleCopHeaderFormat.cs
+++ CodeMaid.IntegrationTests/Formatting/Data/StyleCopHeaderFormat.cs
@@ -1,5 +1,5 @@
 ﻿// <copyright file="StyleCopHeaderFormat.cs" company="CompanyName"> 
-//    Copyright (c) 2014 CompanyName. All rights reserved. 
+//    Copyright (c) 2015 CompanyName. All rights reserved. 
 // </copyright> 
 // <summary>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque convallis non leo nec mollis. Vestibulum ac tortor tincidunt, vestibulum nunc in, egestas arcu.</summary>
 
--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("CodeMaid is an open source Visual Studio extension to cleanup, dig through and simplify our C#, C++, F#, VB, JSON, XAML, XML, ASP, HTML, CSS, LESS, SCSS, JavaScript and TypeScript coding.")]
 [assembly: AssemblyCompany("Steve Cadwallader")]
 [assembly: AssemblyProduct("CodeMaid")]
-[assembly: AssemblyCopyright("Copyright 2007-2014 Steve Cadwallader (LGPL v3)")]
+[assembly: AssemblyCopyright("Copyright 2007-2015 Steve Cadwallader (LGPL v3)")]
 [assembly: CLSCompliant(false)]
 [assembly: ComVisible(false)]
 [assembly: NeutralResourcesLanguage("en-US")]
