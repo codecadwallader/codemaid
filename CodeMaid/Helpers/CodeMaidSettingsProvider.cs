cb112a6f1b34ebe7fd669ff4d78cc2c9879c421d
Steve Cadwallader <steve.cadwallader@gmail.com>
2015-01-02 11:18:32 -0500

    Card 108: Create stub CodeMaidSettingsProvider.

---
+++ CodeMaid/Helpers/CodeMaidSettingsProvider.cs
@@ -0,0 +1,31 @@
+﻿#region CodeMaid is Copyright 2007-2014 Steve Cadwallader.
+
+// CodeMaid is free software: you can redistribute it and/or modify it under the terms of the GNU
+// Lesser General Public License version 3 as published by the Free Software Foundation.
+//
+// CodeMaid is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+// even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details <http://www.gnu.org/licenses/>.
+
+#endregion CodeMaid is Copyright 2007-2014 Steve Cadwallader.
+
+using System.Configuration;
+
+namespace SteveCadwallader.CodeMaid.Helpers
+{
+    /// <summary>
+    /// This class handles customizing the settings persistance.
+    /// </summary>
+    public class CodeMaidSettingsProvider : LocalFileSettingsProvider
+    {
+        public override SettingsPropertyValueCollection GetPropertyValues(SettingsContext context, SettingsPropertyCollection properties)
+        {
+            return base.GetPropertyValues(context, properties);
+        }
+
+        public override void SetPropertyValues(SettingsContext context, SettingsPropertyValueCollection values)
+        {
+            base.SetPropertyValues(context, values);
+        }
+    }
+}
---
+++ CodeMaid/Properties/Settings.cs
@@ -0,0 +1,24 @@
+﻿#region CodeMaid is Copyright 2007-2014 Steve Cadwallader.
+
+// CodeMaid is free software: you can redistribute it and/or modify it under the terms of the GNU
+// Lesser General Public License version 3 as published by the Free Software Foundation.
+//
+// CodeMaid is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+// even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details <http://www.gnu.org/licenses/>.
+
+#endregion CodeMaid is Copyright 2007-2014 Steve Cadwallader.
+
+using SteveCadwallader.CodeMaid.Helpers;
+using System.Configuration;
+
+namespace SteveCadwallader.CodeMaid.Properties
+{
+    /// <summary>
+    /// This partial class instructs the <see cref="Settings"/> class to utilize the <see cref="CodeMaidSettingsProvider"/>.
+    /// </summary>
+    [SettingsProvider(typeof(CodeMaidSettingsProvider))]
+    public sealed partial class Settings
+    {
+    }
+}
