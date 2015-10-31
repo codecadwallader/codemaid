c4d2fa8a4fa407ec944cb34fa3db99e2a0f0df13
Steve Cadwallader <steve.cadwallader@gmail.com>
2015-10-31 06:59:42 -0400

    When checking for an existing open document, also check for a code type for PowerShell support.

--- CodeMaid/Logic/Cleaning/CodeCleanupManager.cs
+++ CodeMaid/Logic/Cleaning/CodeCleanupManager.cs
@@ -115,7 +115,7 @@ namespace SteveCadwallader.CodeMaid.Logic.Cleaning
             if (!_codeCleanupAvailabilityLogic.CanCleanup(projectItem)) return;
 
             // Attempt to open the document if not already opened.
-            bool wasOpen = projectItem.IsOpen[Constants.vsViewKindTextView];
+            bool wasOpen = projectItem.IsOpen[Constants.vsViewKindTextView] || projectItem.IsOpen[Constants.vsViewKindCode];
             if (!wasOpen)
             {
                 try
