00bb1755696c71d247ef13a03bf4560d1ed635cd
Steve Cadwallader <steve.cadwallader@gmail.com>
2016-03-05 14:13:17 -0500

    Add integration tests for PowerShell file headers.

---
+++ CodeMaid.IntegrationTests/Cleaning/Update/FileHeaderPowerShellTests.cs
@@ -0,0 +1,94 @@
+﻿#region CodeMaid is Copyright 2007-2015 Steve Cadwallader.
+
+// CodeMaid is free software: you can redistribute it and/or modify it under the terms of the GNU
+// PowerSheller General Public License version 3 as published by the Free Software Foundation.
+//
+// CodeMaid is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+// even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// PowerSheller General Public License for more details <http://www.gnu.org/licenses/>.
+
+#endregion CodeMaid is Copyright 2007-2015 Steve Cadwallader.
+
+using EnvDTE;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SteveCadwallader.CodeMaid.IntegrationTests.Helpers;
+using SteveCadwallader.CodeMaid.Logic.Cleaning;
+using SteveCadwallader.CodeMaid.Properties;
+
+namespace SteveCadwallader.CodeMaid.IntegrationTests.Cleaning.Update
+{
+    [TestClass]
+    [DeploymentItem(@"Cleaning\Update\Data\FileHeaderPowerShell.ps1", "Data")]
+    [DeploymentItem(@"Cleaning\Update\Data\FileHeaderPowerShell_Cleaned.ps1", "Data")]
+    public class FileHeaderPowerShellTests
+    {
+        #region Setup
+
+        private static FileHeaderLogic _fileHeaderLogic;
+        private ProjectItem _projectItem;
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext testContext)
+        {
+            _fileHeaderLogic = FileHeaderLogic.GetInstance(TestEnvironment.Package);
+            Assert.IsNotNull(_fileHeaderLogic);
+        }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            TestEnvironment.CommonTestInitialize();
+            _projectItem = TestEnvironment.LoadFileIntoProject(@"Data\FileHeaderPowerShell.ps1");
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            TestEnvironment.RemoveFromProject(_projectItem);
+        }
+
+        #endregion Setup
+
+        #region Tests
+
+        [TestMethod]
+        [HostType("VS IDE")]
+        public void CleaningUpdateFileHeaderPowerShell_CleansAsExpected()
+        {
+            Settings.Default.Cleaning_UpdateFileHeaderPowerShell = @"# PowerShell Sample Copyright";
+
+            TestOperations.ExecuteCommandAndVerifyResults(RunUpdateFileHeader, _projectItem, @"Data\FileHeaderPowerShell_Cleaned.ps1");
+        }
+
+        [TestMethod]
+        [HostType("VS IDE")]
+        public void CleaningUpdateFileHeaderPowerShell_DoesNothingOnSecondPass()
+        {
+            Settings.Default.Cleaning_UpdateFileHeaderPowerShell = @"# PowerShell Sample Copyright";
+
+            TestOperations.ExecuteCommandTwiceAndVerifyNoChangesOnSecondPass(RunUpdateFileHeader, _projectItem);
+        }
+
+        [TestMethod]
+        [HostType("VS IDE")]
+        public void CleaningUpdateFileHeaderPowerShell_DoesNothingWhenSettingIsDisabled()
+        {
+            Settings.Default.Cleaning_UpdateFileHeaderPowerShell = null;
+
+            TestOperations.ExecuteCommandAndVerifyNoChanges(RunUpdateFileHeader, _projectItem);
+        }
+
+        #endregion Tests
+
+        #region Helpers
+
+        private static void RunUpdateFileHeader(Document document)
+        {
+            var textDocument = TestUtils.GetTextDocument(document);
+
+            _fileHeaderLogic.UpdateFileHeader(textDocument);
+        }
+
+        #endregion Helpers
+    }
+}
