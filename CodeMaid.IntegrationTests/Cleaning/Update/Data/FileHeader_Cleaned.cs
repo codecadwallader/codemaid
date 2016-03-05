57ba5910cfd1a179f1d70e52fb47f86e77284cb1
Steve Cadwallader <steve.cadwallader@gmail.com>
2016-03-05 07:27:42 -0500

    Add some initial integration tests for the new file header logic.

---
+++ CodeMaid.IntegrationTests/Cleaning/Update/Data/FileHeader_Cleaned.cs
@@ -0,0 +1,11 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="FileHeader.cs" company="CodeMaid">
+//     Sample copyright.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace SteveCadwallader.CodeMaid.IntegrationTests.Cleaning.Update.Data
+{
+    public class FileHeader
+    {
+    }
+}
---
+++ CodeMaid.IntegrationTests/Cleaning/Update/FileHeaderTests.cs
@@ -0,0 +1,104 @@
+﻿#region CodeMaid is Copyright 2007-2015 Steve Cadwallader.
+
+// CodeMaid is free software: you can redistribute it and/or modify it under the terms of the GNU
+// Lesser General Public License version 3 as published by the Free Software Foundation.
+//
+// CodeMaid is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+// even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details <http://www.gnu.org/licenses/>.
+
+#endregion CodeMaid is Copyright 2007-2015 Steve Cadwallader.
+
+using EnvDTE;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SteveCadwallader.CodeMaid.IntegrationTests.Helpers;
+using SteveCadwallader.CodeMaid.Logic.Cleaning;
+using SteveCadwallader.CodeMaid.Properties;
+
+namespace SteveCadwallader.CodeMaid.IntegrationTests.Cleaning.Update
+{
+    [TestClass]
+    [DeploymentItem(@"Cleaning\Update\Data\FileHeader.cs", "Data")]
+    [DeploymentItem(@"Cleaning\Update\Data\FileHeader_Cleaned.cs", "Data")]
+    public class FileHeaderTests
+    {
+        #region Setup
+
+        private static FileHeaderLogic _fileHeaderLogic;
+        private ProjectItem _projectItem;
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext testContext)
+        {
+            _fileHeaderLogic = FileHeaderLogic.GetInstance(TestEnvironment.Package);
+            Assert.IsNotNull(_fileHeaderLogic);
+        }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            TestEnvironment.CommonTestInitialize();
+            _projectItem = TestEnvironment.LoadFileIntoProject(@"Data\FileHeader.cs");
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            TestEnvironment.RemoveFromProject(_projectItem);
+        }
+
+        #endregion Setup
+
+        #region Tests
+
+        [TestMethod]
+        [HostType("VS IDE")]
+        public void CleaningUpdateFileHeader_CleansAsExpected()
+        {
+            Settings.Default.Cleaning_UpdateFileHeader =
+@"//-----------------------------------------------------------------------
+// <copyright file=""FileHeader.cs"" company=""CodeMaid"">
+//     Sample copyright.
+// </copyright>
+//-----------------------------------------------------------------------";
+
+            TestOperations.ExecuteCommandAndVerifyResults(RunUpdateFileHeader, _projectItem, @"Data\FileHeader_Cleaned.cs");
+        }
+
+        [TestMethod]
+        [HostType("VS IDE")]
+        public void CleaningUpdateFileHeader_DoesNothingOnSecondPass()
+        {
+            Settings.Default.Cleaning_UpdateFileHeader =
+@"//-----------------------------------------------------------------------
+// <copyright file=""FileHeader.cs"" company=""CodeMaid"">
+//     Sample copyright.
+// </copyright>
+//-----------------------------------------------------------------------";
+
+            TestOperations.ExecuteCommandTwiceAndVerifyNoChangesOnSecondPass(RunUpdateFileHeader, _projectItem);
+        }
+
+        [TestMethod]
+        [HostType("VS IDE")]
+        public void CleaningUpdateFileHeader_DoesNothingWhenSettingIsDisabled()
+        {
+            Settings.Default.Cleaning_UpdateFileHeader = null;
+
+            TestOperations.ExecuteCommandAndVerifyNoChanges(RunUpdateFileHeader, _projectItem);
+        }
+
+        #endregion Tests
+
+        #region Helpers
+
+        private static void RunUpdateFileHeader(Document document)
+        {
+            var textDocument = TestUtils.GetTextDocument(document);
+
+            _fileHeaderLogic.UpdateFileHeader(textDocument);
+        }
+
+        #endregion Helpers
+    }
+}
