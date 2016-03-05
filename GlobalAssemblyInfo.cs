837c972355b36f3e907b0685523b6d8d054b465e
Steve Cadwallader <steve.cadwallader@gmail.com>
2016-03-05 14:40:57 -0500

    Update all copyright statement headers from 2015 to 2016.

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("CodeMaid is an open source Visual Studio extension to cleanup and simplify our C#, C++, F#, VB, PHP, PowerShell, JSON, XAML, XML, ASP, HTML, CSS, LESS, SCSS, JavaScript and TypeScript coding.")]
 [assembly: AssemblyCompany("Steve Cadwallader")]
 [assembly: AssemblyProduct("CodeMaid")]
-[assembly: AssemblyCopyright("Copyright 2007-2015 Steve Cadwallader (LGPL v3)")]
+[assembly: AssemblyCopyright("Copyright 2007-2016 Steve Cadwallader (LGPL v3)")]
 [assembly: CLSCompliant(false)]
 [assembly: ComVisible(false)]
 [assembly: NeutralResourcesLanguage("en-US")]
