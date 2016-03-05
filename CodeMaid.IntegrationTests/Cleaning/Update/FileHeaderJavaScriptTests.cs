e7b912218be6fb48934420a3c2cd9d720c3951af
Steve Cadwallader <steve.cadwallader@gmail.com>
2016-03-05 13:59:33 -0500

    Add integration tests for JavaScript file headers.

---
+++ CodeMaid.IntegrationTests/Cleaning/Update/FileHeaderJavaScriptTests.cs
@@ -0,0 +1,98 @@
+﻿#region CodeMaid is Copyright 2007-2015 Steve Cadwallader.
+
+// CodeMaid is free software: you can redistribute it and/or modify it under the terms of the GNU
+// Lesser General Public License version 3 as published by the Free Software Foundation.
+//
+// CodeMaid is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+// even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details <http://www.gnu.org/licenses/>.
+
+#endregion CodeMaid is Copyright 2007-2015 Steve Cadwallader.
+
+using EnvDTE;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SteveCadwallader.CodeMaid.IntegrationTests.Helpers;
+using SteveCadwallader.CodeMaid.Logic.Cleaning;
+using SteveCadwallader.CodeMaid.Properties;
+
+namespace SteveCadwallader.CodeMaid.IntegrationTests.Cleaning.Update
+{
+    [TestClass]
+    [DeploymentItem(@"Cleaning\Update\Data\FileHeaderJavaScript.js", "Data")]
+    [DeploymentItem(@"Cleaning\Update\Data\FileHeaderJavaScript_Cleaned.js", "Data")]
+    public class FileHeaderJavaScriptTests
+    {
+        #region Setup
+
+        private static FileHeaderLogic _fileHeaderLogic;
+        private ProjectItem _projectItem;
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext testContext)
+        {
+            _fileHeaderLogic = FileHeaderLogic.GetInstance(TestEnvironment.Package);
+            Assert.IsNotNull(_fileHeaderLogic);
+        }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            TestEnvironment.CommonTestInitialize();
+            _projectItem = TestEnvironment.LoadFileIntoProject(@"Data\FileHeaderJavaScript.js");
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            TestEnvironment.RemoveFromProject(_projectItem);
+        }
+
+        #endregion Setup
+
+        #region Tests
+
+        [TestMethod]
+        [HostType("VS IDE")]
+        public void CleaningUpdateFileHeaderJavaScript_CleansAsExpected()
+        {
+            Settings.Default.Cleaning_UpdateFileHeaderJavaScript =
+@"// JavaScript Sample Copyright
+";
+
+            TestOperations.ExecuteCommandAndVerifyResults(RunUpdateFileHeader, _projectItem, @"Data\FileHeaderJavaScript_Cleaned.js");
+        }
+
+        [TestMethod]
+        [HostType("VS IDE")]
+        public void CleaningUpdateFileHeaderJavaScript_DoesNothingOnSecondPass()
+        {
+            Settings.Default.Cleaning_UpdateFileHeaderJavaScript =
+@"// JavaScript Sample Copyright
+";
+
+            TestOperations.ExecuteCommandTwiceAndVerifyNoChangesOnSecondPass(RunUpdateFileHeader, _projectItem);
+        }
+
+        [TestMethod]
+        [HostType("VS IDE")]
+        public void CleaningUpdateFileHeaderJavaScript_DoesNothingWhenSettingIsDisabled()
+        {
+            Settings.Default.Cleaning_UpdateFileHeaderJavaScript = null;
+
+            TestOperations.ExecuteCommandAndVerifyNoChanges(RunUpdateFileHeader, _projectItem);
+        }
+
+        #endregion Tests
+
+        #region Helpers
+
+        private static void RunUpdateFileHeader(Document document)
+        {
+            var textDocument = TestUtils.GetTextDocument(document);
+
+            _fileHeaderLogic.UpdateFileHeader(textDocument);
+        }
+
+        #endregion Helpers
+    }
+}
