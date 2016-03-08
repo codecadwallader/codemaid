599dfcc765ad6fd0426b9293697d4890779041e2
Willem <willem@quadspeed.com>
2016-03-08 12:23:15 +0100

    Fixed spacing to minimize changes on pull request.

--- CodeMaid/Model/Comments/CommentFormatter.cs
+++ CodeMaid/Model/Comments/CommentFormatter.cs
@@ -2,7 +2,7 @@
 
 // CodeMaid is free software: you can redistribute it and/or modify it under the terms of the GNU
 // Lesser General Public License version 3 as published by the Free Software Foundation.
-// 
+//
 // CodeMaid is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
 // even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 // Lesser General Public License for more details <http://www.gnu.org/licenses/>.
@@ -402,6 +402,7 @@ namespace SteveCadwallader.CodeMaid.Model.Comments
                 foreach (var l in line.Lines)
                 {
                     if (needBreakBefore) NewLine();
+
                     // Parse function returns true if it had to wrap lines. If so, we need to force
                     // a newline before the closing tag.
                     needBreakBefore = Parse(l, indentLevel, xmlTagLength);
--- CodeMaid/Model/Comments/CommentLineXml.cs
+++ CodeMaid/Model/Comments/CommentLineXml.cs
@@ -2,7 +2,7 @@
 
 // CodeMaid is free software: you can redistribute it and/or modify it under the terms of the GNU
 // Lesser General Public License version 3 as published by the Free Software Foundation.
-// 
+//
 // CodeMaid is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
 // even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 // Lesser General Public License for more details <http://www.gnu.org/licenses/>.
