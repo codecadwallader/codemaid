94e94c23c67953f237bbf02c5cef340fa055f294
Steve Cadwallader <steve.cadwallader@gmail.com>
2016-03-19 17:27:30 -0400

    Remove all copyright headers from tops of files.  No longer considered necessary and not the convention anymore these days.  There is a LICENSE.txt in the root with copyright information.

--- CodeMaid/UI/Dialogs/Options/Finding/FindingViewModel.cs
+++ CodeMaid/UI/Dialogs/Options/Finding/FindingViewModel.cs
@@ -1,15 +1,4 @@
-﻿#region CodeMaid is Copyright 2007-2016 Steve Cadwallader.
-
-// CodeMaid is free software: you can redistribute it and/or modify it under the terms of the GNU
-// Lesser General Public License version 3 as published by the Free Software Foundation.
-//
-// CodeMaid is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
-// even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-// Lesser General Public License for more details <http://www.gnu.org/licenses/>.
-
-#endregion CodeMaid is Copyright 2007-2016 Steve Cadwallader.
-
-using SteveCadwallader.CodeMaid.Properties;
+﻿using SteveCadwallader.CodeMaid.Properties;
 
 namespace SteveCadwallader.CodeMaid.UI.Dialogs.Options.Finding
 {
