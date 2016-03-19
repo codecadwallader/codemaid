d2fbd989d07526f9215c3b0efd364233b60d037a
Steve Cadwallader <steve.cadwallader@gmail.com>
2016-03-19 07:24:14 -0400

    Experiment with ExtensibilityTools' auto-sync of .resx and .ico files

---
+++ CodeMaid/source.extension.cs
@@ -0,0 +1,12 @@
+namespace SteveCadwallader.CodeMaid
+{
+	static class Vsix
+	{
+		public const string Id = "4c82e17d-927e-42d2-8460-b473ac7df316";
+		public const string Name = "CodeMaid";
+		public const string Description = "CodeMaid is an open source Visual Studio extension to cleanup and simplify our C#, C++, F#, VB, PHP, PowerShell, JSON, XAML, XML, ASP, HTML, CSS, LESS, SCSS, JavaScript and TypeScript coding.";
+		public const string Version = "0.9.1";
+		public const string Author = "Steve Cadwallader";
+		public const string Tags = "";
+	}
+}
