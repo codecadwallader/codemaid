b607ad5e84fb14aa596cddd9db752d36c1d581db
Steve Cadwallader <steve.cadwallader@gmail.com>
2017-04-09 13:06:09 +0000

    Ongoing exploration.

--- CodeMaid/Integration/Commands/CleanupActiveCodeCommand.cs
+++ CodeMaid/Integration/Commands/CleanupActiveCodeCommand.cs
@@ -1,10 +1,12 @@
 using EnvDTE;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Formatting;
 using SteveCadwallader.CodeMaid.Helpers;
 using SteveCadwallader.CodeMaid.Logic.Cleaning;
 using SteveCadwallader.CodeMaid.Properties;
 using System.ComponentModel.Design;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace SteveCadwallader.CodeMaid.Integration.Commands
 {
@@ -46,13 +48,38 @@ namespace SteveCadwallader.CodeMaid.Integration.Commands
         {
             base.OnExecute();
 
-            //RoslynExploration();
+            //RoslynExploration().Wait();
 
             CodeCleanupManager.Cleanup(Package.ActiveDocument);
         }
 
-        private void RoslynExploration()
+        private async Task RoslynExploration()
         {
+            if (Package.ActiveDocument == null) return;
+
+            //TODO: This didn't actually find the correct document.
+            var roslynDocumentId = Package.Workspace.CurrentSolution.GetDocumentIdsWithFilePath(Package.ActiveDocument.Path).FirstOrDefault();
+            if (roslynDocumentId != null)
+            {
+                var roslynDocument = Package.Workspace.CurrentSolution.GetDocument(roslynDocumentId);
+                if (roslynDocument != null)
+                {
+                    //var editor = await DocumentEditor.CreateAsync(roslynDocument);
+
+                    // This might be close to the equivalent of today's FormatDocument.
+                    var result = await Formatter.FormatAsync(roslynDocument);
+
+                    Package.Workspace.TryApplyChanges(result.Project.Solution);
+
+                    //var newDoc = editor.GetChangedDocument();
+
+                    //var syntaxTree = await roslynDocument.GetSyntaxTreeAsync();
+                    //SyntaxExtensions.NormalizeWhitespace(syntaxTree);
+                    //var newRoot2 = Formatter.Format(syntaxTree, Package.Workspace);
+                    //var newRoot = syntaxRoot.NormalizeWhitespace();
+                }
+            }
+
             foreach (var docId in Package.Workspace.GetOpenDocumentIds())
             {
                 var doc = Package.Workspace.CurrentSolution.GetDocument(docId);
