d1f215850bea103c528ffe42f625c93ffec40c6f
Dmytro Ovcharov <mr.celestian@gmail.com>
2017-08-28 10:12:55 +0300

    Implemented RadialProgressBar in SpadeToolbar

---
+++ CodeMaid/UI/ToolWindows/Spade/WidthConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace SteveCadwallader.CodeMaid.UI.ToolWindows.Spade
+{
+    public class CenterConverter : System.Windows.Data.IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var result = (double)value / 2.0;
+            return result;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
