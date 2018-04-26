80a35f071b7e9087e2d1789873e8efe4ed1cd1da
Willem <willem@quadspeed.com>
2018-04-26 16:04:35 +0200

    Ignore comment lines starting with certain prefixes
    Fixes #347, #350.
    
    Ignore (ie output literally) any comment line starting with certain
    prefixes while formatting. The ignored prefixes are configured in
    the setting file. Updated the comment regex, matching, and formatter.

--- CodeMaid.UnitTests/Formatting/CommentFormatHelper.cs
+++ CodeMaid.UnitTests/Formatting/CommentFormatHelper.cs
@@ -38,11 +38,7 @@ namespace SteveCadwallader.CodeMaid.UnitTests.Formatting
 
         public static string Format(string text, string prefix)
         {
-            var xml = XElement.Parse($"<doc>{text}</doc>");
-            var line = new CommentLineXml(xml);
-            var regex = CodeCommentHelper.GetCommentRegex(CodeLanguage.CSharp, !string.IsNullOrEmpty(prefix));
-            var formatter = new CommentFormatter(line, prefix, 4, regex);
-            return formatter.ToString();
+            return CodeComment.FormatXml(text, prefix);
         }
     }
 }
--- CodeMaid/Properties/Settings.Designer.cs
+++ CodeMaid/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace SteveCadwallader.CodeMaid.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.5.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.6.0.0")]
     public sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -2205,6 +2205,21 @@ namespace SteveCadwallader.CodeMaid.Properties {
             }
             set {
                 this["Feature_SwitchFile"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("<?xml version=\"1.0\" encoding=\"utf-16\"?>\r\n<ArrayOfString xmlns:xsi=\"http://www.w3." +
+            "org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">\r\n  <s" +
+            "tring>ReSharper disable </string>\r\n  <string>ReSharper enable </string>\r\n</Array" +
+            "OfString>")]
+        public global::System.Collections.Specialized.StringCollection Formatting_IgnoreLinesStartingWith {
+            get {
+                return ((global::System.Collections.Specialized.StringCollection)(this["Formatting_IgnoreLinesStartingWith"]));
+            }
+            set {
+                this["Formatting_IgnoreLinesStartingWith"] = value;
             }
         }
     }
