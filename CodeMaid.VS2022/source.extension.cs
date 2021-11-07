a3a7ff1182308fd734ce112b39f4b3956c6dd1ba
Steve Cadwallader <steve.cadwallader@gmail.com>
2021-11-07 17:03:45 -0500

    Separate ID and Name for VS2022.

--- CodeMaid.VS2022/source.extension.cs
+++ CodeMaid.VS2022/source.extension.cs
@@ -2,8 +2,8 @@ namespace SteveCadwallader.CodeMaid
 {
     internal static class Vsix
     {
-        public const string Id = "4c82e17d-927e-42d2-8460-b473ac7df316";
-        public const string Name = "CodeMaid";
+        public const string Id = "9079e73d-3fbb-4e07-8dab-f44fa5d8e8b5";
+        public const string Name = "CodeMaid VS2022";
         public const string Description = "CodeMaid is an open source Visual Studio extension to cleanup and simplify our C#, C++, F#, VB, PHP, PowerShell, R, JSON, XAML, XML, ASP, HTML, CSS, LESS, SCSS, JavaScript and TypeScript coding.";
         public const string Language = "en-US";
         public const string Version = "12.0";
